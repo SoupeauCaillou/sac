5800ff793c3d616626d1ccc2c3186d9571dcd02e
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@linphone.org>
2012-01-13 19:09:51 +0100

    Rendering: enable blending

--- systems/RenderingSystem.cpp
+++ systems/RenderingSystem.cpp
@@ -123,6 +123,8 @@ void RenderingSystem::DoUpdate(float dt) {
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glDisable(GL_DEPTH_TEST);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	glUseProgram(defaultProgram);
 	float ratio = h / (float)w ;
