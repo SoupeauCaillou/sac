d89cc767599d400afb744a3d3e804357b60b2b36
Pierre-Eric Pelloux-Prayer <pelloux@gmail.com>
2012-03-10 14:06:23 +0100

    Music Android

--- systems/SoundSystem.cpp
+++ systems/SoundSystem.cpp
@@ -2,30 +2,49 @@
 
 INSTANCE_IMPL(SoundSystem);
 
-SoundSystem::SoundSystem() : ComponentSystemImpl<SoundComponent>("sound") {
+SoundSystem::SoundSystem() : ComponentSystemImpl<SoundComponent>("sound"), nextValidRef(1) {
 }
 
 void SoundSystem::init() {
-
+	/* preload sound effects */
 }
 
-SoundRef SoundSystem::loadSoundFile(const std::string& assetName) {
+SoundRef SoundSystem::loadSoundFile(const std::string& assetName, bool music) {
 	if (assetSounds.find(assetName) != assetSounds.end())
 		return assetSounds[assetName];
 
-	if (!assetLoader)
-		return 0;
-
-	// TODO
-
-	//sounds[nextValidRef] = texture;
+#ifdef ANDROID
+	int soundID = androidSoundAPI->loadSound(assetName, music);
+#endif
+	sounds[nextValidRef] = soundID;
 	assetSounds[assetName] = nextValidRef;
+	
+	LOGW("Sound : %s -> %d -> %d", assetName.c_str(), nextValidRef, soundID);
 
 	return nextValidRef++;
 }
 
-
-
 void SoundSystem::DoUpdate(float dt) {
 	/* play component with a valid sound ref */
+	for(ComponentIt it=components.begin(); it!=components.end(); ++it) {
+		Entity a = (*it).first;
+		SoundComponent* rc = (*it).second;
+		if (rc->sound != InvalidSoundRef) {
+			if (!rc->started) {
+				LOGW("sound started (%d)", rc->sound);
+				androidSoundAPI->play (sounds[rc->sound], (rc->type == SoundComponent::MUSIC));
+				rc->started = true;
+			} else if (rc->type == SoundComponent::MUSIC) {
+				float newPos = androidSoundAPI->musicPos(sounds[rc->sound]);
+				if (newPos == rc->position && newPos >= 0.99) {
+					LOGW("sound ended (%d)", rc->sound);
+					rc->position = 0;
+					rc->sound = InvalidSoundRef;
+					rc->started = false;
+				} else {
+					rc->position = newPos;
+				}
+			}
+		}	
+	}
 }
--- systems/SoundSystem.h
+++ systems/SoundSystem.h
@@ -16,16 +16,51 @@ typedef int SoundRef;
 
 #define InvalidSoundRef -1
 
-class NativeSoundAssetLoader {
-	public:
-		virtual char* decompressOggSound(const std::string& assetName) = 0;
+#ifdef ANDROID
+#include <jni.h>
+struct JavaSoundAPI {
+	JNIEnv* env;
+	jobject assetManager;
+	jclass javaSoundApi;
+	jmethodID jloadSound;
+	jmethodID jplaySound;
+	jmethodID jmusicPos;
+	jmethodID jpauseSounds;
+	jmethodID jresumeSounds;
+	
+	int loadSound(const std::string& assetName, bool music) {
+		jstring asset = env->NewStringUTF(assetName.c_str());
+		int result = env->CallStaticIntMethod(javaSoundApi, jloadSound, assetManager, asset, music);
+		return result;
+	}
+	
+	int play(int soundId, bool music) {
+		return env->CallStaticIntMethod(javaSoundApi, jplaySound, soundId, music);
+	}
+	
+	float musicPos(int soundId) {
+		return env->CallStaticFloatMethod(javaSoundApi, jmusicPos, soundId);
+	}
+	
+	void pauseAll() {
+		env->CallStaticObjectMethod(javaSoundApi, jpauseSounds);
+	}
+	
+	void resumeAll() {
+		env->CallStaticObjectMethod(javaSoundApi, jresumeSounds);
+	}
 };
 
+#endif
+
 struct SoundComponent {
+	SoundComponent() : sound(InvalidSoundRef), position(0), started(false) {}
 	SoundRef sound;
+	enum { MUSIC, EFFECT } type;
 	float position;
 	bool repeat; /* si repeat est faux: qd le son a été joué en plein, on passe sound à InvalidSoundRef */
 	/* openal specific datas : openAL source ? */
+	bool started;
 };
 
 #define theSoundSystem SoundSystem::GetInstance()
@@ -36,19 +71,20 @@ UPDATABLE_SYSTEM(Sound)
 public:
 void init();
 
-SoundRef loadSoundFile(const std::string& assetName);
-
-void setNativeSoundAssetLoader(NativeSoundAssetLoader* ptr) { assetLoader = ptr; }
+SoundRef loadSoundFile(const std::string& assetName, bool music);
 
 private:
 /* textures cache */
 SoundRef nextValidRef;
 std::map<std::string, SoundRef> assetSounds;
 #ifdef ANDROID
-std::map<SoundRef, int/* mettre le bon type ici */> sounds;
+std::map<SoundRef, int> sounds;
 #else
 std::map<SoundRef, int/* mettre le bon type ici */> sounds;
 #endif
 
-NativeSoundAssetLoader* assetLoader;
+#ifdef ANDROID
+public:
+	JavaSoundAPI* androidSoundAPI;
+#endif
 };
