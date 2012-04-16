023d3a26d0f30981115c6dbe11d2fad76acc9ddf
Gautier Pelloux-Prayer <gpelloux@gmail.com>
2012-04-16 09:20:27 +0200

    Systeme morphing

--- systems/MorphingSystem.cpp
+++ systems/MorphingSystem.cpp
@@ -1,1 +1,23 @@
+#include "MorphingSystem.h"
+
+INSTANCE_IMPL(MorphingSystem);
+
+MorphingSystem::MorphingSystem() : ComponentSystemImpl<MorphingComponent>("Morphing") { }
+
+void MorphingSystem::DoUpdate(float dt) {
+	for(std::map<Entity, MorphingComponent*>::iterator jt=components.begin(); jt!=components.end(); ++jt) {
+		MorphingComponent* m = (*jt).second;
+
+		if ((!m->active && m->activationTime<=0) || m->activationTime>m->timing) {
+			m->active = false;
+			m->activationTime = 0;
+			continue;
+		}
+		if (m->active) {
+			m->activationTime += dt;
+			m->value = m->activationTime/m->timing;
+			m->pos = MathUtil::Lerp(m->from, m->to, m->value);
+		}
+	}
+}
 
--- systems/MorphingSystem.h
+++ systems/MorphingSystem.h
@@ -1,1 +1,25 @@
+#pragma once
+
+#include "base/MathUtil.h"
+
+#include "System.h"
+
+struct MorphingComponent {
+	MorphingComponent() : active(false), value(0), timing(0), activationTime(0), from(Vector2::Zero), to(Vector2::Zero), pos(Vector2::Zero) {}
+	bool active;
+
+	Vector2 from, to, pos;
+
+	float value;
+	float activationTime;
+	float timing;
+};
+
+#define theMorphingSystem MorphingSystem::GetInstance()
+#define MORPHING(entity) theMorphingSystem.Get(entity)
+
+UPDATABLE_SYSTEM(Morphing)
+
+};
+
 
