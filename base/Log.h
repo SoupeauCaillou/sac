7c7d3a5743546ad5c7efd7a180d1e89bb33b8305
Pierre-Eric Pelloux-Prayer <pelloux@gmail.com>
2012-05-28 00:03:39 +0200

    Add ERROR log level

--- base/Log.h
+++ base/Log.h
@@ -7,10 +7,14 @@
 
 #define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "tilematchC", __VA_ARGS__))
 #define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN, "tilematchC", __VA_ARGS__))
+#define LOGE(...) ((void)__android_log_print(ANDROID_LOG_ERRO, "tilematchC", __VA_ARGS__))
+
 #else
 #include <cstdio>
 extern bool __log_enabled;
 
-#define LOGI(...) (__log_enabled & printf("[%s] ", __PRETTY_FUNCTION__) & printf(__VA_ARGS__) & printf("\n"))
-#define LOGW(...) (__log_enabled & printf("[%s] ", __PRETTY_FUNCTION__) & printf(__VA_ARGS__) & printf("\n"))
+#define LOGE(...) (__log_enabled & printf("ERRO-[%s] ", __PRETTY_FUNCTION__) & printf(__VA_ARGS__) & printf("\n"))
+#define LOGW(...) (__log_enabled & printf("WARN-[%s] ", __PRETTY_FUNCTION__) & printf(__VA_ARGS__) & printf("\n"))
+#define LOGI(...) (__log_enabled & printf("INFO-[%s] ", __PRETTY_FUNCTION__) & printf(__VA_ARGS__) & printf("\n"))
+
 #endif
