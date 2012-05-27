3335952715842f480ce71e9cdd7dd22f21875ae7
Gautier Pelloux-Prayer <gpelloux@gmail.com>
2012-05-28 00:07:25 +0200

    correction màj table sql pour la pub

--- api/AdAPI.h
+++ api/AdAPI.h
@@ -1,7 +1,9 @@
 #pragma once
 
+#include <base/Log.h>
+
 class AdAPI {
     public:
-        virtual void showAd() {}
+        virtual void showAd() { LOGI("affichage de la pub !"); }
         virtual bool done() { return true; }
 };
