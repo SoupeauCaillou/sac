16fce63466097629f52ce6280ee8ed25d553135d
Pierre-Eric Pelloux-Prayer <pierre-eric@lanedo.com>
2012-06-26 23:36:25 +0200

    Improve NmaeInput on desktop

--- api/linux/NameInputAPILinuxImpl.cpp
+++ api/linux/NameInputAPILinuxImpl.cpp
@@ -19,17 +19,29 @@
 #include "NameInputAPILinuxImpl.h"
 #include "base/Log.h"
 #include <iostream>
+#include "../../systems/TextRenderingSystem.h"
+#include "../../systems/ButtonSystem.h"
 
 void NameInputAPILinuxImpl::show() {
-    __log_enabled = false;
-    std::cout << "Enter your name (oui Gautier, j'ai tout cassé)" << std::endl;
+    __log_enabled = false;    
+    TEXT_RENDERING(title)->hide = TEXT_RENDERING(nameEdit)->hide = false;
+    RENDERING(background)->hide = false;
+    // BUTTON(button)->enabled = true;
+    textIsReady = false;
 }
 
 bool NameInputAPILinuxImpl::done(std::string& name) {
-    std::cin >> name;
-    return true;
+    if (textIsReady && TEXT_RENDERING(nameEdit)->text.length() > 0) {
+	    name = TEXT_RENDERING(nameEdit)->text;
+	    return true;
+    }
+    textIsReady = false;
+    return false;
 }
 
 void NameInputAPILinuxImpl::hide() {
     __log_enabled = true;
+    TEXT_RENDERING(title)->hide = TEXT_RENDERING(nameEdit)->hide = true;
+    RENDERING(background)->hide = true;
+    // BUTTON(button)->enabled = false;
 }
--- api/linux/NameInputAPILinuxImpl.h
+++ api/linux/NameInputAPILinuxImpl.h
@@ -19,10 +19,14 @@
 #pragma once
 
 #include "../NameInputAPI.h"
+#include "../../base/EntityManager.h"
 
 class NameInputAPILinuxImpl : public NameInputAPI {
     public:
         void show();
         bool done(std::string& name);
         void hide();
+        
+        Entity title, nameEdit, background;
+        bool textIsReady;
 };
