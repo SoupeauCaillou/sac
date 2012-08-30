802fada44b6df72a5ed48bdc25e708bdef6fa8e5
Pierre-Eric Pelloux-Prayer <pierre-eric@damsy.net>
2012-08-30 19:51:24 +0200

    color: color can be named, then built from a name
    Closes: #290

--- base/Color.cpp
+++ base/Color.cpp
@@ -2,13 +2,32 @@
 
 #include "MathUtil.h"
 #include <cstring>
+#include <map>
+#include "Log.h"
+
+static std::map<std::string, Color> name2Color;
 
 Color Color::random() {
 	return (Color( MathUtil::RandomFloat(), MathUtil::RandomFloat(), MathUtil::RandomFloat()));
 }
 
+void Color::nameColor(const Color& c, const std::string& name) {
+	if (!name2Color.insert(std::make_pair(name, c)).second) {
+		LOGW("Warning, color with name '%s' already existed. Replaced anyway", name.c_str());
+	}
+}
+
 Color::Color(float _r, float _g, float _b, float _a):
 	r(_r), g(_g), b(_b), a(_a) {
+}
+
+Color::Color(const std::string& name) {
+	std::map<std::string, Color>::iterator it = name2Color.find(name);
+	if (it != name2Color.end()) {
+		*this = it->second;
+	} else {
+		r = g = b = a = 1.0;
+	}
 }
 
 Color Color::operator*(float s) const {
--- base/Color.h
+++ base/Color.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <string>
+
 struct Color {
 	union {
 		struct {
@@ -11,13 +13,17 @@ struct Color {
 	};
 
  	static Color random();
-
+	static void nameColor(const Color& c, const std::string& name);
+	
 	Color(float _r=1.0, float _g=1.0, float _b=1.0, float _a=1.0);
+	Color(const std::string& name);
 
-     Color operator*(float s) const;
+    Color operator*(float s) const;
      Color operator+(const Color& c) const;
 
      bool operator!=(const Color& c) const;
      bool operator<(const Color& c) const;
+     
+     
 };
 
