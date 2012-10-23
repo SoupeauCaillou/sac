4834e312ca592f72a0cf6beb792ea9140b144710
Pierre-Eric Pelloux-Prayer <pierre-eric@damsy.net>
2012-10-23 23:43:30 +0200

    SuccessAPI moved to heriswap

--- api/android/SuccessAPIAndroidImpl.cpp
+++ api/android/SuccessAPIAndroidImpl.cpp
@@ -11,20 +11,20 @@ void SuccessAPIAndroidImpl::uninit() {
 void SuccessAPIAndroidImpl::successCompleted(const char* description, unsigned long successId) {
 	SuccessAPI::successCompleted(description, successId);
 	// android spec stuff
-	jclass c = env->FindClass("net/damsy/soupeaucaillou/api/SuccessAPI");
+	jclass c = env->FindClass("net/damsy/soupeaucaillou/heriswap/api/SuccessAPI");
 	jmethodID mid = (env->GetStaticMethodID(c, "unlockAchievement", "(I)V"));
 	int sid = (int) successId;
 	env->CallStaticVoidMethod(c, mid, sid);
 }
 
 void SuccessAPIAndroidImpl::openLeaderboard(int mode, int diff) {
-	jclass c = env->FindClass("net/damsy/soupeaucaillou/api/SuccessAPI");
+	jclass c = env->FindClass("net/damsy/soupeaucaillou/heriswap/api/SuccessAPI");
 	jmethodID mid = env->GetStaticMethodID(c, "openLeaderboard", "(II)V");
 	env->CallStaticVoidMethod(c, mid, mode, diff);
 }
 
 void SuccessAPIAndroidImpl::openDashboard() {
-	jclass c = env->FindClass("net/damsy/soupeaucaillou/api/SuccessAPI");
+	jclass c = env->FindClass("net/damsy/soupeaucaillou/heriswap/api/SuccessAPI");
 	jmethodID mid = env->GetStaticMethodID(c, "openDashboard", "()V");
 	env->CallStaticVoidMethod(c, mid);
 }
