7f43b63219beea448eec2e2028f0709da1d56e99
Gautier Pelloux-Prayer <gautier@damsy.net>
2012-10-30 00:21:00 +0100

    Added tinyxml in localize API. Can change language on pc!

--- api/linux/LocalizeAPILinuxImpl.cpp
+++ api/linux/LocalizeAPILinuxImpl.cpp
@@ -22,21 +22,52 @@
 #include <locale.h>
 #endif
 
+#include <iostream> // à virer
+
+#include <tinyxml2.h>
+
+#include "../../base/Log.h"
+
 void LocalizeAPILinuxImpl::init() {
 	#ifndef EMSCRIPTEN
-	#ifdef LOCALEDIR
-	bindtextdomain("heriswap", LOCALEDIR);
-	textdomain("heriswap");
+	char* lang = strdup(getenv("LANG"));
+	lang[2] = '\0';
+	
+	std::string filename = DATADIR;
+	filename += "../res/values";
+	
+	if (lang != "en")
+		filename += "-" + std::string(lang);
+	filename += "/strings.xml";
+	
+	tinyxml2::XMLDocument doc;
+	
+	if (doc.LoadFile(filename.c_str())) {
+		LOGE("can't open xml file %s\n", filename.c_str());
+		return;
+	}
+
+	tinyxml2::XMLHandle hDoc(&doc);
+	tinyxml2::XMLElement * pElem;
+
+	pElem = hDoc.FirstChildElement().ToElement();
+	tinyxml2::XMLHandle hRoot(pElem);
+
+	pElem = hRoot.FirstChildElement().ToElement();
+	for (pElem; pElem; pElem = pElem->NextSiblingElement()) {
+		_idToMessage[pElem->Attribute("name")] = pElem->GetText();
+	}
+	
 	#endif
-	#endif
+
 }
 
 #define _(STRING)    gettext(STRING)
 
 std::string LocalizeAPILinuxImpl::text(const std::string& s, const std::string& spc) {
-#if defined(LOCALEDIR) && !defined(EMSCRIPTEN)
+#ifdef EMSCRIPTEN
 	return _(spc.c_str());
 #else
-	return spc;
+	return _idToMessage[s];
 #endif
 }
