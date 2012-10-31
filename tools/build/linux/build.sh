7cb4ba93de31ef46c560ac251aee1d36b86ebd0e
Gautier Pelloux-Prayer <gautier@damsy.net>
2012-10-31 21:12:54 +0100

    added clean in linux build

--- tools/build/linux/build.sh
+++ tools/build/linux/build.sh
@@ -1,3 +1,15 @@
-#!/bin/sh
+#! /bin/sh -x
+if [ $# -eq 1 ] && [ "$0" = "./build.sh" ]; then
+	echo "cleaning"
+	rm -r C* c* l* M* s* t* 2>/dev/null
+fi
+	
 cmake ../..
 make -j4
+
+echo "launch it ? (y/N) "
+read confirm
+
+if [ "$confirm" = "y" ]; then
+	./linux/heriswap --verbose
+fi
