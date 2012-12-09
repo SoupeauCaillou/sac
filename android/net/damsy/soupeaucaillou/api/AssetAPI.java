545165ab015ba79fedb2e8acc6faf67396861ead
Pierre-Eric Pelloux-Prayer <pierre-eric@damsy.net>
2012-12-10 00:04:48 +0100

    asset/android: load pvm/pvr in chunks

--- android/net/damsy/soupeaucaillou/api/AssetAPI.java
+++ android/net/damsy/soupeaucaillou/api/AssetAPI.java
@@ -1,22 +1,56 @@
 package net.damsy.soupeaucaillou.api;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 import android.content.res.AssetManager;
+import android.util.Log;
 
 public class AssetAPI {
 	// -------------------------------------------------------------------------
 	// AssetAPI
 	// -------------------------------------------------------------------------
 	static public byte[] assetToByteArray(AssetManager mgr, String assetName) {
+		if (assetName.endsWith(".pkm") || assetName.endsWith(".pvr"))
+			return chunkedAssetToByteArray(mgr, assetName);
 		try {
 			InputStream stream = mgr.open(assetName);
 			byte[] data = new byte[stream.available()];
 			stream.read(data);
 			return data;
 		} catch (Exception exc) {
-			//NOLOGLog.e(HeriswapActivity.Tag, exc.toString());
+			Log.e("sac", "load asset error: " + exc.toString(), exc);
 			return null;
 		}
+	}
+	
+	static byte[] chunkedAssetToByteArray(AssetManager mgr, String assetName) {
+		int totalLength = 0;
+		int idx = 0;
+		do {
+			try {
+				InputStream stream = mgr.open(assetName + ".0" + Integer.toString(idx));
+				totalLength += stream.available();
+				idx++;
+			} catch (IOException exc) {
+				break;
+			}
+		
+		} while (true);
+		Log.i("sac", "chunked asset : " + idx + " chunk, total size: " + totalLength);
+		byte[] data = new byte[totalLength];
+		int offset = 0;
+		for (int i=0; i<idx; i++) {
+			try {
+				InputStream stream = mgr.open(assetName + ".0" + Integer.toString(i));
+				int l = stream.available();
+				stream.read(data, offset, l);
+				offset += l;
+			} catch (IOException exc) {
+				Log.e("sac", "load asset error: " + exc.toString(), exc);
+				return null;
+			}
+		}
+		return data;
 	}
 }
