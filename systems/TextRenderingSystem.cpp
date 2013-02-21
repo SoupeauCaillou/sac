ce5569f43cde7f5d6f909b2c583f60a323ff62b2
Pierre-Eric Pelloux-Prayer <pierre-eric@damsy.net>
2013-02-21 19:49:57 +0100

    Use MurmurHash util

--- systems/TextRenderingSystem.cpp
+++ systems/TextRenderingSystem.cpp
@@ -9,7 +9,7 @@
 #include "TransformationSystem.h"
 #include "RenderingSystem.h"
 
-static unsigned int MurmurHash2 ( const void * key, int len, unsigned int seed );
+#include "util/MurmurHash.h"
 
 struct CacheKey {
     Color color;
@@ -122,7 +122,7 @@ void TextRenderingSystem::DoUpdate(float dt) {
         // compute cache entry
         if (trc->blink.onDuration == 0) {
             unsigned keySize = key.populate(trc);
-            unsigned hash = MurmurHash2(&key, keySize, 0x12345678);
+            unsigned hash = MurmurHash::compute(&key, keySize, 0x12345678);
             std::map<Entity, unsigned int>::iterator c = cache.find(entity);
             if (c != cache.end()) {
                 if (hash == (*c).second)
@@ -324,58 +324,6 @@ void TextRenderingSystem::DeleteEntity(Entity e) {
 	}
 	tc->drawing.clear();
 	theEntityManager.DeleteEntity(e);
-}
-
-// MurmurHash2, by Austin Appleby
-static unsigned int MurmurHash2 ( const void * key, int len, unsigned int seed )
-{
- // 'm' and 'r' are mixing constants generated offline.
- // They're not really 'magic', they just happen to work well.
-
- const unsigned int m = 0x5bd1e995;
- const int r = 24;
-
- // Initialize the hash to a 'random' value
-
- unsigned int h = seed ^ len;
-
- // Mix 4 bytes at a time into the hash
-
- const unsigned char * data = (const unsigned char *)key;
-
- while(len >= 4)
- {
-     unsigned int k = *(unsigned int *)data;
-
-     k *= m;
-     k ^= k >> r;
-     k *= m;
-
-     h *= m;
-     h ^= k;
-
-     data += 4;
-     len -= 4;
- }
-
- // Handle the last few bytes of the input array
-
- switch(len)
- {
- case 3: h ^= data[2] << 16;
- case 2: h ^= data[1] << 8;
- case 1: h ^= data[0];
-         h *= m;
- };
-
- // Do a few final mixes of the hash to ensure the last few
- // bytes are well-incorporated.
-
- h ^= h >> 13;
- h *= m;
- h ^= h >> 15;
-
- return h;
 }
 
 #ifdef INGAME_EDITORS
