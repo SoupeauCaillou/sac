c9789026e1a974b41e802d2a248dec850a2a5e54
Pierre-Eric Pelloux-Prayer <pierre-eric@damsy.net>
2013-02-21 19:49:07 +0100

    Add murmur hash util

---
+++ util/MurmurHash.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <glog/logging.h>
+
+class MurmurHash {
+    public:
+        #ifdef DEBUG
+        static inline unsigned staticHash(const char *s, unsigned value) {
+            LOG_IF(FATAL, compute(s, strlen(s), 0) != value) << "Invalid precalculated hash value. Expected: " << compute(s, strlen(s), 0)  << ". Actual: " << value;
+            return value;
+        }
+        #else
+        #define staticHash(s,v) (v)
+        #endif
+
+        static unsigned int compute( const void * key, int len, unsigned int seed );
+};
