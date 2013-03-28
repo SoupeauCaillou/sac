f1c0b435584049e7b79a49756ebcc5aeebd5b1eb
Pierre-Eric Pelloux-Prayer <pierre-eric@damsy.net>
2013-03-28 22:51:39 +0100

    tools: cleanup atlas generation scripts

---
+++ tools/generate_atlas.sh
@@ -0,0 +1,92 @@
+#!/bin/sh
+
+# path need :
+# location of texture_packer (your_build_dir/sac/build/cmake)
+# location of PVRTexToolCL (need to install it from http://www.imgtec.com/powervr/insider/powervr-pvrtextool.asp)
+# location of etc1tool (android-sdk-linux/tools)
+
+reset="[0m"
+red="[1m[31m"
+green="[1m[32m"
+yellow="[1m[33m"
+
+if [ $# != 1 ]; then
+	echo "${red}Usage: $0 image_folder${reset}"
+	exit 1
+fi
+
+############# STEP 0: verify env
+if hash texture_packer 2>/dev/null; then
+    echo "texture_packer found!"
+else
+    echo "${red}texture_packer must be in PATH$reset"
+    exit 1
+fi
+
+hasPVRTool=false
+if hash PVRTexToolCL 2>/dev/null; then
+    echo "PVRTexToolCL found!"
+    hasPVRTool=true
+else
+    echo "${yellow}Warning: PVRTexToolCL not found -> compressed format won't be created$reset"
+fi
+
+SCRIPT=$(readlink -f $0)
+SAC_TOOLS_DIR=$(dirname $SCRIPT)
+TEMP_FOLDER=/tmp/$1
+
+############# STEP 1: preparation
+echo "${green}Step #1: prepare temp folder ($TEMP_FOLDER)${reset}"
+if [ -d "$TEMP_FOLDER" ]; then
+    rm -r $TEMP_FOLDER
+fi
+mkdir $TEMP_FOLDER
+
+############# STEP 2: create an optimized copy of each image
+printf "${green}Step #2: create optimized image${reset}"
+for file in `ls $1/*png`
+do
+    printf "${green}.${reset}"
+    used_rect=`${SAC_TOOLS_DIR}/texture_packer/tiniest_rectangle.py ${file}`
+    ww=`echo $used_rect | cut -d, -f1`
+    hh=`echo $used_rect | cut -d, -f2`
+    xx=`echo $used_rect | cut -d, -f3`
+    yy=`echo $used_rect | cut -d, -f4`
+    convert -crop ${ww}x${hh}+${xx}+${yy} ${file} PNG32:$TEMP_FOLDER/`basename ${file}`
+done
+echo "${green}Done${reset}"
+
+############# STEP 3: fit all image in one texture
+# Remark: we feed texture_packer with optimized (cropped) images
+echo "${green}Step #3: compute atlas placement${reset}"
+optimized_image_list=$(ls $TEMP_FOLDER/*png)
+
+############# STEP 4: place image in atlas
+# Remark: we first cd into original image folder
+echo "${green}Step #4: compose atlas using individual images coords${reset}"
+oldpwd=$PWD
+cd $1
+texture_packer $optimized_image_list | ${SAC_TOOLS_DIR}/texture_packer/texture_packer.sh $1
+cd $oldpwd
+
+############# STEP 5: create png version of the atlas
+echo "${green}Step #5: create png version${reset}"
+convert /tmp/$1.png -alpha extract -depth 8 ../assets/$1_alpha.png
+# mais pourquoi j'ai fait ca ?convert ../assets/$1_alpha.png -background white -flatten +matte -depth 8 ../assets/$1_alpha.png
+convert /tmp/$1.png -background white -alpha off -type TrueColor PNG24:../assetspc/$1.png
+
+if  $hasPVRTool ; then
+    echo "${green}Step #6: create PVR version${reset}"
+    PVRTexToolCL -f OGLPVRTC4 -yflip0 -i ../assetspc/$1.png -p -pvrlegacy -m -o /tmp/$1.pvr
+    split -d -b 1024K /tmp/$1.pvr $1.pvr.
+    mv $1.pvr.0* ../assets/
+
+    echo "${green}Step #7: create ETC version${reset}"
+    PVRTexToolCL -f ETC -yflip0 -i ../assetspc/$1.png -q 3 -m -pvrlegacy -o /tmp/$1.pkm
+
+    # PVRTexToolCL ignore name extension
+    split -d -b 1024K /tmp/$1.pvr $1.pkm.
+    mv $1.pkm.0* ../assets/
+fi
+
+cp /tmp/$1.atlas ../assets/
--- tools/texture_packer/texture_packer.sh
+++ tools/texture_packer/texture_packer.sh
@@ -1,17 +1,27 @@
-#!/bin/sh
+#!/bin/bash
+
+reset="[0m"
+red="[1m[31m"
+blue="[1m[34m"
 
 if [ $# -ne 1 ]
 then
-	echo "Usage: texture_packer atlas_to_create"
+	echo "Usage: $0 atlas_to_create"
 	exit 1
 fi
 
+SCRIPT=$(readlink -f $0)
+SAC_TOOLS_TEXTURE_DIR=$(dirname $SCRIPT)
+
 output=/tmp/${1}.png
-#output_alpha=/tmp/${1}_alpha.png
 desc=/tmp/${1}.atlas
 tmp_image=/tmp/tmp___.png
 
-rm ${desc}
+if [ -f "${desc}" ]; then
+    rm ${desc}
+fi
+
+COUNTER=0
 
 # Take the output of texture_packer as input and then use image magick to compose the atlas
 while read data; do
@@ -20,21 +30,29 @@ while read data; do
 		# create image
 		w=`echo $data | cut -d: -f2 | cut -d, -f1`
 		h=`echo $data | cut -d, -f2`
-		echo "Atlas size: $w x $h"
 
 		convert -size ${w}x${h} xc:transparent ${output}
-		#convert -size ${w}x${h} xc:transparent ${output_alpha}
-		echo "$w,$h" > ${desc}
-        #echo "# image,original width, original height, redux offset x, redux offset y, pox x in atlas, pos y in atlas, width, height, rotate[, opaque box min x, min y, max x, max y]" > ${desc}
+		echo "\tatlas_size=$w,$h" > ${desc}
 	else
+        #read input from pipe
+        #image is an absolute path
 		image=`echo $data | cut -d, -f1`
 		x=`echo $data | cut -d, -f2`
 		y=`echo $data | cut -d, -f3`
 		w=`echo $data | cut -d, -f4`
 		h=`echo $data | cut -d, -f5`
 		rot=`echo $data | cut -d, -f6`
+
+        # extract image name (without extension)
         base=`basename ${image} .png`
+        # find out original (unoptimized) image size
         real_size=`identify ${base}.png | cut -d\  -f3 | sed 's/x/,/'`
+        # compute optimized area in image
+        used_rect=`${SAC_TOOLS_TEXTURE_DIR}/tiniest_rectangle.py ${base}.png`
+        used_rect_w=`echo ${used_rect} | cut -d, -f1`
+        used_rect_h=`echo ${used_rect} | cut -d, -f2`
+        used_rect_x=`echo ${used_rect} | cut -d, -f3`
+        used_rect_y=`echo ${used_rect} | cut -d, -f4`
 
 		if [ ${rot} -ne "0" ]
 		then
@@ -43,25 +61,26 @@ while read data; do
 			cp ${image} ${tmp_image}
 		fi
 
-		echo "Adding ${image} at ${w}x${h}+${x}+${y} (rotation:${rot})"
+		echo "   ${blue}Adding ${image} at ${w}x${h}+${x}+${y} (rotation:${rot})${reset}"
 		# copy a first version, 2 pixel taller/larger (used as a blend-compatible border)
 		convert -geometry `expr ${w} + 2;`x`expr ${h} + 2;`+`expr ${x} - 1;`+`expr ${y} - 1;` -compose Copy -composite $output $tmp_image $output
 		# copy the real image
-        #convert -geometry ${w}x${h}+${x}+${y} -compose clear -composite $output $tmp_image $output
 		convert -geometry ${w}x${h}+${x}+${y} -compose Copy -composite $output $tmp_image $output
-		#convert -geometry ${w}x${h}+${x}+${y} -composite $output_alpha $tmp_image $output_alpha
-		#image=`basename ${image} .png`
-		
-		#largest_rectangle script 
-		opaque=`../../sac/tools/texture_packer/largest_rectangle.py ${image}`
-        used_rect=`../../sac/tools/texture_packer/tiniest_rectangle.py ${base}.png`
-        used_rect_x=`echo ${used_rect} | cut -d, -f3`
-        used_rect_y=`echo ${used_rect} | cut -d, -f4`
 
-		if [ -n "$opaque" ]; then
-			echo "`basename ${base}`,${real_size},${used_rect_x},${used_rect_y},${x},${y},${w},${h},${rot},${opaque}" >> ${desc}
-		else
-			echo "`basename ${base}`,${real_size},${used_rect_x},${used_rect_y},${x},${y},${w},${h},${rot}" >> ${desc}
-		fi
+
+		#largest_rectangle script
+		opaque=`$SAC_TOOLS_TEXTURE_DIR/largest_rectangle.py ${image}`
+
+        echo "[image$COUNTER]" >> ${desc}
+        echo "name=${base}" >> ${desc}
+        echo "original_size=${real_size}" >> ${desc}
+        echo "position_in_atlas=${x},${y}" >> ${desc}
+        echo "size_in_atlas=${w},${h}" >> ${desc}
+        echo "crop_offset=${used_rect_x},${used_rect_y}" >> ${desc}
+        echo "rotated=${rot}" >> ${desc}
+        if [ -n "$opaque" ]; then
+            echo "opaque_rect=${opaque}" >> ${desc}
+        fi
+        let COUNTER=COUNTER+1
 	fi
 done
