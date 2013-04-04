3aabab0fe963d1a4ad52e05c4e5a26e588f2d4cc
Pierre-Eric Pelloux-Prayer <pierre-eric@damsy.net>
2013-04-05 00:19:20 +0200

    Split Android touch driver code to new files

---
+++ android/AndroidNativeTouchState.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "base/TouchInputManager.h"
+class GameHolder;
+
+struct AndroidNativeTouchState : public NativeTouchState {
+	GameHolder* holder;
+
+	AndroidNativeTouchState(GameHolder* h);
+
+    int maxTouchingCount();
+
+	bool isTouching (int index, glm::vec2* windowCoords) const;
+};
