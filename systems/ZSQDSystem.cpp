68c22641731ce88fd6ebc98fdec6095298b868d7
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2013-05-01 16:18:40 +0200

    fix zsqd system

--- systems/ZSQDSystem.cpp
+++ systems/ZSQDSystem.cpp
@@ -10,15 +10,16 @@ ZSQDSystem::ZSQDSystem() : ComponentSystemImpl<ZSQDComponent>("ZSQD") {
     ZSQDComponent zc;
     componentSerializer.add(new Property<float>("friction_coeff", OFFSET(frictionCoeff, zc), 0.0001f));
     componentSerializer.add(new Property<float>("max_speed", OFFSET(maxSpeed, zc), 0.0001f));
+    componentSerializer.add(new Property<float>("new_direction_coeff", OFFSET(newDirectionCoeff, zc), 0.0001f));
 }
 
 void ZSQDSystem::DoUpdate(float dt) {
     FOR_EACH_ENTITY_COMPONENT(ZSQD, a, zc)
         //decrease current speed
-        zc->currentSpeed = glm::max(zc->currentSpeed - zc->frictionCoeff * zc->maxSpeed, 0.f);
+        zc->currentSpeed = glm::max(zc->currentSpeed - zc->frictionCoeff * zc->maxSpeed * dt, 0.f);
         //if someone added some new directions, update speed and direction
         if (zc->directions.size() > 0) {
-            DEBUG_LOGI(zc->directions.size() << " more directions");
+            //DEBUG_LOGI(zc->directions.size() << " more directions");
 
             //calculate the average new direction from the whole new inputs
             glm::vec2 newDir(0, 0);
@@ -28,35 +29,38 @@ void ZSQDSystem::DoUpdate(float dt) {
                 newDir.x += it.x / norm;
                 newDir.y += it.y / norm;
             }
-            //normalize the extra direction
+            //normalize the new direction
             if (newDir != glm::vec2(0.f, 0.f))
                 newDir = glm::normalize(newDir);
 
-            DEBUG_LOGI("current dir: " << zc->currentDirection.x << ", " << zc->currentDirection.y << " at speed " << zc->currentSpeed << " and new direction is: " << newDir.x << ", " << newDir.y << " at speed " << zc->maxSpeed);
+            DEBUG_LOGI("current dir: " << zc->currentDirection << " at speed " << zc->currentSpeed
+                << "\nAnd new direction is: " << newDir << " at speed " << zc->maxSpeed);
 
+            //calculate the weight of the new direction
+            float weight = zc->newDirectionCoeff;
+            if (newDir == glm::vec2(0.f, 0.f))
+                weight = zc->newDirectionCoeff * 3;
+            else if (zc->currentDirection == glm::vec2(0.f, 0.f))
+                weight = 1.;
 
-            ///////////////////////method1
-            float weight = (newDir == glm::vec2(0.f, 0.f)) ? 0.3 : 0.1;
-            zc->currentDirection = newDir * weight + zc->currentDirection * (1-weight);
+            //update the direction with ponderated directions
+            zc->currentDirection = newDir * weight + zc->currentDirection * (1 - weight);
 
-            ///////////////////////method2
-            //current direction is the average from itself and the new direction. The coefficient of each is the speed associated
-            zc->currentDirection = (zc->currentDirection * zc->currentSpeed + newDir * (zc->maxSpeed -  zc->currentSpeed)) / (zc->maxSpeed);
+            //currentDirection norm must be <= 1
+            if (glm::length(zc->currentDirection) > 1.f)
+                zc->currentDirection = glm::normalize(zc->currentDirection);
 
-            zc->currentDirection = (glm::length(zc->currentDirection) < 0.0001f) ?
-                glm::vec2(0.f, 0.f)
-                : glm::normalize(zc->currentDirection);
-
+            //reset speed to maxSpeed, because we had new inputs
             zc->currentSpeed = zc->maxSpeed;
-
 
             zc->directions.clear();
         }
 
+        //if we are moving, update the position
         if (zc->currentSpeed > 0.f && zc->currentDirection != glm::vec2(0.f, 0.f)) {
-            //DEBUG_LOGI("adding movement: " << zc->currentDirection.x << ", " << zc->currentDirection.y << " at speed " << zc->currentSpeed);
             TRANSFORM(a)->position += zc->currentDirection * zc->currentSpeed * dt;
         }
+
     }
 }
 
@@ -67,5 +71,7 @@ void ZSQDSystem::addEntityPropertiesToBar(Entity entity, TwBar* bar) {
 
     TwAddVarRW(bar, "frictionCoeff", TW_TYPE_FLOAT, &zc->frictionCoeff, "group=ZSQD precision=2 step=0,1");
     TwAddVarRW(bar, "maxSpeed", TW_TYPE_FLOAT, &zc->maxSpeed, "group=ZSQD precision=2 step=0,1");
+    TwAddVarRW(bar, "newDirectionCoeff", TW_TYPE_FLOAT, &zc->newDirectionCoeff, "group=ZSQD precision=2 step=0,1");
+
 }
 #endif
--- systems/ZSQDSystem.h
+++ systems/ZSQDSystem.h
@@ -6,19 +6,31 @@
 #include <list>
 
 struct ZSQDComponent {
-    ZSQDComponent() : currentSpeed(0.f), maxSpeed(1.f), frictionCoeff(0.1f) {}
+    ZSQDComponent() : currentSpeed(0.f), maxSpeed(1.f), frictionCoeff(0.1f), newDirectionCoeff(0.01f) {}
 
     //new directions are added in this list
     std::list<glm::vec2> directions;
+
+    //use this when you are using a vector, eg not dependent from the point position
     void addDirectionVector(glm::vec2 v) { directions.push_front(v); }
+    //use this when the position vector is the target point
     void addDirectionPoint(Entity e, glm::vec2 pos) { directions.push_front(pos - TRANSFORM(e)->worldPosition); }
 
+    //currentDirection of the entity, should not be changed
     glm::vec2 currentDirection;
+    //same as above
     float currentSpeed;
+
+    //max speed of the entity in range [0, +oo[
     float maxSpeed;
 
+    //how much the speed will decrease per sec
     //in range [0, 1], relative to maxSpeed (0.1 is 10% of maxSpeed)
     float frictionCoeff;
+
+    //how much the new direction will influences the direction (0: don't influence at all, 1: it's the new direction)
+    //in range [0, 1]
+    float newDirectionCoeff;
 };
 
 #define theZSQDSystem ZSQDSystem::GetInstance()
