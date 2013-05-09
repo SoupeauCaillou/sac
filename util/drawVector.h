d19273c1e4b27dd5bbd482a234fe1de2546f5821
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2013-05-09 23:55:55 +0200

    disable logs

--- util/drawVector.h
+++ util/drawVector.h
@@ -1,6 +1,8 @@
 #include "systems/TransformationSystem.h"
 #include "systems/RenderingSystem.h"
 
+#include "base/Log.h"
+
 #include <glm/glm.hpp>
 #include <glm/gtx/vector_angle.hpp>
 
@@ -11,12 +13,12 @@ Entity drawVector(const glm::vec2& position, const glm::vec2& size) {
 
     TRANSFORM(vector)->size = glm::vec2(glm::length(size), 0.1f);
     TRANSFORM(vector)->rotation = glm::orientedAngle(glm::vec2(1.f, 0.f), glm::normalize(size));
-    LOGW("normalize : " << glm::normalize(size).x << "," << glm::normalize(size).y << " : " << glm::orientedAngle(glm::vec2(1.f, 0.f), glm::normalize(size)));
-    
+    //LOGV(1, "normalize : " << glm::normalize(size).x << "," << glm::normalize(size).y << " : " << glm::orientedAngle(glm::vec2(1.f, 0.f), glm::normalize(size)));
+
     float y = TRANSFORM(vector)->size.x * glm::sin(TRANSFORM(vector)->rotation);
     float x = TRANSFORM(vector)->size.x * glm::cos(TRANSFORM(vector)->rotation);
     TRANSFORM(vector)->position = position + glm::vec2(x, y) / 2.f;
-    LOGW("Vector : " << TRANSFORM(vector)->position.x << "," << TRANSFORM(vector)->position.y << " : " << TRANSFORM(vector)->size.x << "," << TRANSFORM(vector)->size.y << " : " << TRANSFORM(vector)->rotation);
+    //LOGV(1, "Vector " << vector << ": " << TRANSFORM(vector)->position.x << "," << TRANSFORM(vector)->position.y << " : " << TRANSFORM(vector)->size.x << "," << TRANSFORM(vector)->size.y << " : " << TRANSFORM(vector)->rotation);
     TRANSFORM(vector)->z = 1;
     RENDERING(vector)->show = true;
 
