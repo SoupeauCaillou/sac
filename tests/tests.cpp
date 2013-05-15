eaf0202b1add30c5c94752ee186f114821c02339
Pierre-Eric Pelloux-Prayer <pierre-eric@damsy.net>
2013-05-15 16:32:25 +0200

    tests: do not assert on LOGF. Otherwise sac_tests is removed by cmake

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -7,6 +7,8 @@
 int main(int argc, char **) {
     SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
 	EntityManager::CreateInstance();
+    AssertOnFatal = false;
+
     if (argc == 1)
         logLevel = LogVerbosity::FATAL;
     else
