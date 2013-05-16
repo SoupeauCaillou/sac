7f543784c5acdc5baea3708bbea8126ae439e82e
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2013-05-16 22:02:27 +0200

    add some tests which are yet failing

--- tests/systems/TestAnchorSystem.cpp
+++ tests/systems/TestAnchorSystem.cpp
@@ -1,5 +1,7 @@
 #include <UnitTest++.h>
 
+#include <glm/gtx/vector_angle.hpp>
+#include <glm/gtc/constants.hpp>
 #include <glm/glm.hpp>
 #include "systems/AnchorSystem.h"
 #include "systems/TransformationSystem.h"
@@ -27,4 +29,113 @@ TEST(AnchorParentingChain)
     CHECK_CLOSE(3, TRANSFORM(e[2])->position.x, 0.001);
     TransformationSystem::DestroyInstance();
     AnchorSystem::DestroyInstance();
+}
+
+static const std::vector<glm::vec2> rectanglePoints(const TransformationComponent* tc1) {
+    std::vector<glm::vec2> res;
+
+    //NW
+    res.push_back( tc1->position + glm::rotate(glm::vec2(- tc1->size.x, tc1->size.y) * .5f, tc1->rotation));
+    //NE
+    res.push_back( tc1->position + glm::rotate(glm::vec2(tc1->size.x, tc1->size.y) * .5f, tc1->rotation));
+    //SW
+    res.push_back( tc1->position + glm::rotate(glm::vec2(- tc1->size.x, - tc1->size.y) * .5f, tc1->rotation));
+    //SE
+    res.push_back( tc1->position + glm::rotate(glm::vec2(tc1->size.x, - tc1->size.y) * .5f, tc1->rotation));
+
+    return res;
+}
+
+TEST(TransformationTestCentralRotation90Degrees) {
+    AnchorSystem::CreateInstance();
+    TransformationSystem::CreateInstance();
+    Entity e = 1;
+    theTransformationSystem.Add(e);
+    theAnchorSystem.Add(e);
+
+    TRANSFORM(e)->position = glm::vec2(1, 0);
+    TRANSFORM(e)->size = glm::vec2(2, 1);
+
+    ANCHOR(e)->parent = e;
+    ANCHOR(e)->rotation = 0.5f * glm::pi<float>();
+    theTransformationSystem.Update(1.0f);
+    theAnchorSystem.Update(1.0f);
+
+    glm::vec2 expected[4] = { glm::vec2(.5,-1), glm::vec2(.5, 1),
+                              glm::vec2(1.5, -1), glm::vec2(1.5, 1) };
+
+    auto values = rectanglePoints(TRANSFORM(e));
+
+    for (int i = 0; i < 4; ++i) {
+       CHECK_CLOSE(expected[i].x, values[i].x, 0.0001);
+       CHECK_CLOSE(expected[i].y, values[i].y, 0.0001);
+    }
+
+
+    AnchorSystem::DestroyInstance();
+    TransformationSystem::DestroyInstance();
+}
+
+TEST(TransformationTestNorthWestRotation90Degrees) {
+    TransformationSystem::CreateInstance();
+    AnchorSystem::CreateInstance();
+    Entity e = 1;
+    theTransformationSystem.Add(e);
+    theAnchorSystem.Add(e);
+
+    TRANSFORM(e)->position = glm::vec2(1, 0);
+    TRANSFORM(e)->size = glm::vec2(2, 1);
+
+    ANCHOR(e)->parent = e;
+    ANCHOR(e)->position = glm::vec2(-1, .5);
+    ANCHOR(e)->rotation = .5f * glm::pi<float>();
+
+    theTransformationSystem.Update(1.0f);
+    theAnchorSystem.Update(1.0f);
+
+    glm::vec2 expected[4] = { glm::vec2(0, .5), glm::vec2(0, 2.5),
+                              glm::vec2(1, 0.5), glm::vec2(1, 2.5) };
+
+    auto values = rectanglePoints(TRANSFORM(e));
+
+    for (int i = 0; i < 4; ++i) {
+       CHECK_CLOSE(expected[i].x, values[i].x, 0.0001);
+       CHECK_CLOSE(expected[i].y, values[i].y, 0.0001);
+    }
+
+
+    AnchorSystem::DestroyInstance();
+    TransformationSystem::DestroyInstance();
+}
+
+TEST(TransformationTestSouthRotation45Degrees) {
+    AnchorSystem::CreateInstance();
+    TransformationSystem::CreateInstance();
+    Entity e = 1;
+    theTransformationSystem.Add(e);
+    theAnchorSystem.Add(e);
+
+    TRANSFORM(e)->position = glm::vec2(1, 0);
+    TRANSFORM(e)->size = glm::vec2(2, 1);
+
+    ANCHOR(e)->parent = e;
+    ANCHOR(e)->position = glm::vec2(0, -0.5);
+    ANCHOR(e)->rotation = .25f * glm::pi<float>();
+
+    theTransformationSystem.Update(1.0f);
+    theAnchorSystem.Update(1.0f);
+
+    glm::vec2 expected[4] = { glm::vec2(-.41, -.5), glm::vec2(1, 0.91),
+                              glm::vec2(0.29, -1.2), glm::vec2(1.7, 0.20) };
+
+    auto values = rectanglePoints(TRANSFORM(e));
+
+    for (int i = 0; i < 4; ++i) {
+       CHECK_CLOSE(expected[i].x, values[i].x, 0.01);
+       CHECK_CLOSE(expected[i].y, values[i].y, 0.01);
+    }
+
+
+    AnchorSystem::DestroyInstance();
+    TransformationSystem::DestroyInstance();
 }
