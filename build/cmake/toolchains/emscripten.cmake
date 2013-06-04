c553e160343204083f154600bec74babf6a40910
Pierre-Eric Pelloux-Prayer <pierre-eric@damsy.net>
2013-06-04 23:00:31 +0200

    emscripten: remove -g flags, and remove compressed texture from assts

--- build/cmake/toolchains/emscripten.cmake
+++ build/cmake/toolchains/emscripten.cmake
@@ -3,11 +3,9 @@ ADD_DEFINITIONS(-DSAC_WEB=1 -DSAC_USE_VBO=1 -DSAC_ENABLE_LOG=1)
 set(CMAKE_C_COMPILER emcc)
 set(CMAKE_CXX_COMPILER emcc)
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Qunused-arguments -O0 --jcache")
-set(C_FLAGS_DEBUG "-g")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Qunused-arguments -O2 --jcache")
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Qunused-arguments -std=c++0x -O0 --jcache")
-set(CXX_FLAGS_DEBUG "-g")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Qunused-arguments -std=c++0x -O2 --jcache")
 
 add_definitions(-D__GCC_HAVE_SYNC_COMPARE_AND_SWAP_1 -D__GCC_HAVE_SYNC_COMPARE_AND_SWAP_2 -D__GCC_HAVE_SYNC_COMPARE_AND_SWAP_4 -D__GCC_HAVE_SYNC_COMPARE_AND_SWAP_8)
 
@@ -24,12 +22,13 @@ function (postbuild_specific_actions)
         COMMAND rm -rf assets
         COMMAND mkdir assets
         COMMAND cp -r ${PROJECT_SOURCE_DIR}/assets/* ${PROJECT_SOURCE_DIR}/assetspc/* assets
+        COMMAND rm assets/*pvr* assets/*pkm*
         WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
         COMMENT "Building 'assets' folder"
     )
     add_custom_command(
         TARGET ${EXECUTABLE_NAME} POST_BUILD
-        COMMAND EMCC_DEBUG=1 emcc -O2 -s WARN_ON_UNDEFINED_SYMBOLS=1 -s ALLOW_MEMORY_GROWTH=1
+        COMMAND EMCC_DEBUG=1 emcc --llvm-lto 1 -O2 -s WARN_ON_UNDEFINED_SYMBOLS=1 -s ALLOW_MEMORY_GROWTH=1
         ${CMAKE_BINARY_DIR}/${EXECUTABLE_NAME} -o ${PROJECT_NAME}.html
          --preload-file assets
 
