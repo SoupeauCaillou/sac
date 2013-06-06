54527ed0fe6f1a1c7832e86197d4f3616a597680
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2013-06-06 19:44:06 +0200

    Changed directory syntax / finished the script

--- tools/build/push-build-on-website.sh
+++ tools/build/push-build-on-website.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 #where the script is
 whereAmI=$(cd "$(dirname "$0")" && pwd)
@@ -14,7 +14,7 @@ source ../coolStuff.sh
 
 #how to use the script
 export USAGE="\
-Copy an emscripten build (game.html and game.data) on a distant server, will the following syntax: gameName-currentDate
+Copy an emscripten build (game.html and game.data) on a distant server, will the following syntax: gameName-branchName-commitId-currentDate
 \t$0 -d build-directory -[specifics_options] [and their values]"
 export OPTIONS="\
 -h|--help: show this help
@@ -75,26 +75,32 @@ on server, with format {gameName}-{date}"
     SERVER=$(echo $USER_SERVER | cut -d '@' -f2 | cut -d '/' -f1)
 
     if [ -z "$DIRECTORY" ] || [ ! -e "$DIRECTORY" ]; then
-	error_and_quit "Directory '$DIRECTORY' does not exist"
+	   error_and_quit "Directory '$DIRECTORY' does not exist"
     fi
-    DIRECTORY_FORMATTED=$(echo $USER_SERVER | cut -d '/' -f2-)/$(echo $gameName-$(date +"%Y-%m-%d-%H-%M"))
+    DATE=$(date +"%Y.%m.%d.%H.%M")
+    GIT_BRANCH=$(cd $rootPath && git rev-parse --abbrev-ref HEAD)
+    GIT_COMMIT=$(cd $rootPath && git rev-parse --short HEAD)
+    DIRECTORY_FORMATTED=$(echo $USER_SERVER | cut -d '/' -f2-)/$(echo $gameName-$GIT_BRANCH-$GIT_COMMIT-$DATE)
 
 ######### 4 :  check if the necessary files are in folder.. #########
-    HTML_FILE=$(find $DIRECTORY -maxdepth 1 -name '*.html')
+    HTML_FILE=$(find $DIRECTORY -maxdepth 1 -name '*.html' -printf "%f\n")
     if [ -z "$HTML_FILE" ]; then
         error_and_quit "Could not locate any .html file in $DIRECTORY"
     fi
-    DATA_FILE=$(find $DIRECTORY -maxdepth 1 -name '*.data')
+    DATA_FILE=$(find $DIRECTORY -maxdepth 1 -name '*.data' -printf "%f\n")
     if [ -z "$DATA_FILE" ]; then
         error_and_quit "Could not locate any .data file in $DIRECTORY"
     fi
 ######### 5 : Push content on website. #########
+    info "User is '$USER' on server '$SERVER'. The directory name will be '$DIRECTORY_FORMATTED'. Asking for ftp password."
+
     TMP_DIR=/tmp/$PPID
     mkdir $TMP_DIR
-    cp $HTML_FILE $DATA_FILE $TMP_DIR
+    cp $DIRECTORY/$HTML_FILE $DIRECTORY/$DATA_FILE $TMP_DIR
     lftp -u $USER $SERVER -e "mirror -R $TMP_DIR $DIRECTORY_FORMATTED && exit"
     rm -rf $TMP_DIR
 ######### 6 : (optional) Open navigator. #########
     if [ "$LAUNCH_ON_END" = "1" ]; then
-        iceweasel $SERVER/$DIRECTORY_FORMATTED/
+        info "Launching iceweasel for page $SERVER/$DIRECTORY_FORMATTED/$HTML_FILE..."
+        iceweasel $SERVER/$DIRECTORY_FORMATTED/$HTML_FILE
     fi
