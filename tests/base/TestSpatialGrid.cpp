35e9a0a9f15c95892300e8692e2032106ea7c1cc
Jordane Pelloux-Prayer <jordane@damsy.net>
2013-06-07 16:58:21 +0200

    Add a test for ring and fix another

--- tests/base/TestSpatialGrid.cpp
+++ tests/base/TestSpatialGrid.cpp
@@ -7,7 +7,7 @@ TEST(GridNeighborsAtCenter)
 {
 	SpatialGrid grid(5, 5);
 	GridPos p(0, 0);
-	auto neighbors = grid.getNeighbors(p);
+	auto neighbors = grid.getNeighbors(p, false);
 	CHECK_EQUAL(6u, neighbors.size());
 	for(auto& n: neighbors) {
 		CHECK_EQUAL(1u, SpatialGrid::ComputeDistance(p, n));
@@ -18,7 +18,7 @@ TEST(GridNeighborsAtTopLeft)
 {
 	SpatialGrid grid(5, 5);
 	GridPos p(-1,-2);
-	auto neighbors = grid.getNeighbors(p);
+	auto neighbors = grid.getNeighbors(p, false);
 	CHECK_EQUAL(2u, neighbors.size());
 	for(auto& n: neighbors) {
 		CHECK_EQUAL(1u, SpatialGrid::ComputeDistance(p, n));
@@ -31,4 +31,13 @@ TEST(PositionToGridPosCenter)
     GridPos p = grid.positionToGridPos(glm::vec2(0, 0));
     CHECK_EQUAL(0, p.q);
     CHECK_EQUAL(0, p.r);
+}
+
+TEST(RingAt2OfCenter)
+{
+	SpatialGrid grid(5,5);
+	GridPos p = grid.positionToGridPos(glm::vec2(0, 0));
+	std::vector<GridPos> i = grid.ringFinder(p, 2, false);
+	CHECK_EQUAL(12, (int)i.size());
+
 }
