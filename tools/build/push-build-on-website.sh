966ce180dfb470abd1be8bb695be839579ffc228
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2013-06-19 22:01:08 +0200

    add option -i for push-build script, to generate index.html automatically (redirection to the last build automatically)

--- tools/build/push-build-on-website.sh
+++ tools/build/push-build-on-website.sh
@@ -22,6 +22,7 @@ Copy an emscripten build (game.html and game.data) on a distant server, will the
 export OPTIONS="\
 -h|--help: show this help
 \t-d: ${red}[REQUIRED]${yellow}specify the build directory to copy.
+\t-i: create an empty index.html file at the server root directory. ${red}[Warning: will overwrite any existing index.html!]${yellow}
 \t-p: view the result in iceweasel.
 \t-s: specify user/server address. Default value is 'soupeaucaillou@soupeaucaillou.com/prototypes'"
 export EXAMPLE="\
@@ -41,6 +42,7 @@ on server, with format {gameName}-{date}"
     USER_SERVER='soupeaucaillou@soupeaucaillou.com/prototypes'
     DIRECTORY=''
     LAUNCH_ON_END=0
+    CREATE_INDEX=0
     while [ "$1" != "" ]; do
         case $1 in
             "-h" | "--help")
@@ -50,6 +52,9 @@ on server, with format {gameName}-{date}"
         		shift
         		DIRECTORY=${whereUserIs}/${1}
         		;;
+            "-i")
+                CREATE_INDEX=1
+                ;;
             "-p")
                 LAUNCH_ON_END=1
                 ;;
@@ -85,8 +90,7 @@ on server, with format {gameName}-{date}"
     DATE=$(date +"%Y.%m.%d.%H.%M")
     GIT_BRANCH=$(cd $rootPath && git rev-parse --abbrev-ref HEAD)
     GIT_COMMIT=$(cd $rootPath && git rev-parse --short HEAD)
-    DIRECTORY_FORMATTED=$(echo $USER_SERVER | cut -d '/' -f2-)/$(echo $gameName-$GIT_BRANCH-$GIT_COMMIT-$DATE)
-
+    DIRECTORY_FORMATTED=$(echo $USER_SERVER | cut -d '/' -f2-)$(echo $gameName-$GIT_BRANCH-$GIT_COMMIT-$DATE)
 ######### 4 :  check if the necessary files are in folder.. #########
     info "Checking if $DIRECTORY contains a .html file and a .data file..."
     HTML_FILE=$(find $DIRECTORY -maxdepth 1 -name '*.html' -printf "%f\n")
@@ -97,15 +101,32 @@ on server, with format {gameName}-{date}"
     if [ -z "$DATA_FILE" ]; then
         error_and_quit "Could not locate any .data file in $DIRECTORY"
     fi
-######### 5 : Push content on website. #########
-    info "User is '$USER' on server '$SERVER'. The directory name will be '$DIRECTORY_FORMATTED'. Asking for ftp password..."
 
-    TMP_DIR=/tmp/$PPID
-    mkdir $TMP_DIR
+######### 5 : Prepare the datas #########
+    TMP_DIR=$(mktemp -d)
     cp $DIRECTORY/$HTML_FILE $DIRECTORY/$DATA_FILE $TMP_DIR
-    lftp -u $USER $SERVER -e "mirror -R $TMP_DIR $DIRECTORY_FORMATTED && exit"
-    rm -rf $TMP_DIR
-######### 6 : (optional) Open navigator. #########
+
+    #create a simple index.html which just does a redirection...
+    if [ "$CREATE_INDEX" = "1" ]; then
+        TMP_FILE=$(mktemp)
+        echo "<html><head><title>Redirection...</title>
+        <meta http-equiv=\"refresh\" content=\"0; URL=$(echo $DIRECTORY_FORMATTED | cut -d"/" -f2-)/$HTML_FILE\">
+        </head><body></body></html>" > $TMP_FILE
+    fi
+
+######### 6 : Prepare the request. #########
+    REQUEST_LFTP="mirror -R $TMP_DIR $DIRECTORY_FORMATTED"
+    if [ "$CREATE_INDEX" = "1" ]; then
+        REQUEST_LFTP=$REQUEST_LFTP" && put $TMP_FILE -o $DIRECTORY_FORMATTED/../index.html"
+    fi
+    REQUEST_LFTP=$REQUEST_LFTP" && exit"
+
+######### 7 : Push content on website. #########
+    info "User is '$USER' on server '$SERVER'. The directory name will be '$DIRECTORY_FORMATTED'. Asking for ftp password..."
+    lftp -u $USER $SERVER -e "$REQUEST_LFTP"
+
+######### 8 : (optional) Open navigator. #########
+
     if [ "$LAUNCH_ON_END" = "1" ]; then
         info "Launching iceweasel for page $SERVER/$DIRECTORY_FORMATTED/$HTML_FILE..."
         iceweasel $SERVER/$DIRECTORY_FORMATTED/$HTML_FILE &
