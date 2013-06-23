55c6bd7647049257b413e6afd33cb82fe5bebe66
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2013-06-23 19:13:56 +0200

    [Script] Generate state machine is now a bit smarter :-)

--- tools/generate_state_machine.sh
+++ tools/generate_state_machine.sh
@@ -43,10 +43,47 @@ export SAC_EXAMPLE="\
 
     rootPath=$whereAmI"/../.."
 ######### 2 :
-get_return_within_DoUpdate() {
-    grep -n 'return Scene::' $file | cut -f1 -d:
+#first arg: method name
+#second arg: file name
+#third arg: output file
+get_return_within_method() {
+    info "Treating file $2, looking for method '$1'..."
+    method_line_start=$(grep -n "Scene::Enum $1(" $2 | cut -f1 -d:)
+
+    lines=$(tail -n +$method_line_start $2)
+    IFS=$'\n'
+
+    opening_brace_count=0
+    closing_brace_count=0
+    for line in $lines; do
+        #there is a specific case. FOR_EACH macros are using a '{' implicitely, so we need to count them too
+        opening_brace_count=$(expr $opening_brace_count + $(echo $line | tr -c -d '{' | wc -c))
+        closing_brace_count=$(expr $closing_brace_count + $(echo $line | tr -c -d '}' | wc -c))
+        # echo "still in doupdate:" $line $opening_brace_count $closing_brace_count
+
+        if (grep 'return ' -q <<< $line); then
+            next_state=$(echo $line | sed -e 's/.*return //g' -e 's/Scene:://g')
+
+            #if we are using return like 'return nextState(some params);', we have to check the nextState function!
+            if (grep '(' -q <<< $next_state); then
+                function_name=$(echo $next_state | cut -d'(' -f1)
+                # echo "oops, need to check function $function_name too"
+                get_return_within_method "$function_name" $2 $3
+            else
+                # echo "there is a return other here: next_state is $next_state"
+                echo "$state -> $next_state" >> $3
+            fi
+        fi
+
+
+        if [ $opening_brace_count = $closing_brace_count ]; then
+            # echo "same count of braces, end of doupdate function"
+            break;
+        fi
+    done
 }
 
+    info "todo: chercher le 'if' au dessus du return, afin de le mettre en condition de l'automate..." $red
 
 
     temp_file=$(mktemp)
@@ -56,25 +93,27 @@ get_return_within_DoUpdate() {
     states=$(cd $statesDirectory && echo * | sed -e 's/.cpp//g' -e 's/Scenes.h//' -e 's/Scene$/ /g' -e 's/Scene / /g')
     echo "States are: $states"
 
+    #there are some specifics states more: the fade out/in states. There are registered in sources/#GameName#Game.cpp
+    gameName=$(cat $rootPath/CMakeLists.txt | grep 'project(' | cut -d '(' -f2 | tr -d ')')
+
+    OLD_IFS=$IFS
+    IFS=$'\n'
+    for fade_state in $(grep 'registerState' $rootPath/sources/${gameName}Game.cpp | grep 'Scene::CreateFadeSceneHandler'); do
+        fade_state_name=$(echo $fade_state | sed 's/.*(Scene:://' | cut -d ',' -f1)
+        fade_state_next_state=$(echo $fade_state | cut -d ',' -f5 | cut -d ')' -f1 | sed 's/.*Scene:://')
+        echo "$fade_state_name -> $fade_state_next_state" >> $temp_file
+    done
+    IFS=$OLD_IFS
+
     #for each state, get its DoUpdate function; and particulary all the 'return' inside it
     for state in $states; do
         # echo $state
         file=$statesDirectory/${state}Scene.cpp
-
-        #first we keep the lines of the 'return'
-        next_states_line=$(get_return_within_DoUpdate $file)
-
-        for next_state_line in $next_states_line; do
-            #then we parse these lines to keep only the next scene name
-            next_state=$(sed -n "${next_state_line}p" $file | sed -e 's/.*return Scene:://' | tr -d ';  \t')
-            echo "$state -> $next_state" >> $temp_file
-
-            #todo: chercher le 'if' au dessus du return, afin de le mettre en condition de l'automate...
-        done
+        get_return_within_method 'update' $file $temp_file
     done
 
     #just for debug
-    cat $temp_file | sort -u
+    # cat $temp_file | sort -u
 
     output_file=/tmp/sac_graphfiz.png
 
