e295477a9fbd2c46cf9896bbdb59cff24d45a79a
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2013-06-23 19:36:16 +0200

    [Build][Emscripten] Increase memory size

--- build/cmake/toolchains/emscripten.cmake
+++ build/cmake/toolchains/emscripten.cmake
@@ -32,7 +32,7 @@ function (postbuild_specific_actions)
     )
     add_custom_command(
         TARGET ${EXECUTABLE_NAME} POST_BUILD
-        COMMAND EMCC_DEBUG=1 emcc --llvm-lto 1 -O2 -s TOTAL_MEMORY=67108864 -s VERBOSE=1 -s WARN_ON_UNDEFINED_SYMBOLS=1
+        COMMAND EMCC_DEBUG=1 emcc --llvm-lto 1 -O2 -s TOTAL_MEMORY=1073741824 -s VERBOSE=1 -s WARN_ON_UNDEFINED_SYMBOLS=1
         ${CMAKE_BINARY_DIR}/${EXECUTABLE_NAME} -o ${PROJECT_NAME}.html
          --preload-file assets
         COMMAND sed -i "s/Emscripten-Generated\ Code/${PROJECT_NAME} - ${DATE_VAR} - ${COMMIT_VAR}/" ${PROJECT_NAME}.html
