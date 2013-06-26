125fbfe091cfbcf7eae6e74e29ce5ad277152fc5
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2013-06-26 23:29:42 +0200

    [Script] Updated build-android script

--- tools/build/build-android.sh
+++ tools/build/build-android.sh
@@ -2,10 +2,46 @@
 
 #how to use the script
 export PLATFORM_OPTIONS="\
-\t\t- c: compile java code (.java)
-\t\t- i: install on device
-\t\t- r: run on device
-\t\t- l: run adb logcat"
+\t\t-j|java: compile java code (.java)
+\t\t-i|install: install on device
+\t\t-l|-log: run adb logcat"
+
+
+parse_arguments() {
+    UPDATE_JAVA=0
+    INSTALL_ON_DEVICE=0
+    RUN_LOGCAT=0
+    while [ "$1" != "" ]; do
+        case $1 in
+            #ignore higher level commands
+            "-h" | "-help" | "-release" | "-debug")
+                # info "Ignoring '$1' (low lvl)"
+                ;;
+            #ignore higher level commands
+            "--c" | "--cmakeconfig" | "--t" | "--target")
+                # info "Ignoring '$1' and its arg '$2' (low lvl)"
+                shift
+                ;;
+            "-j" | "-java")
+                UPDATE_JAVA=1
+                ;;
+            "-i" | "-install")
+                INSTALL_ON_DEVICE=1
+                ;;
+            "-l" | "-log")
+                RUN_LOGCAT=1
+                ;;
+            --*)
+                info "Unknown option '$1', ignoring it and its arg '$2'" $red
+                shift
+                ;;
+            -*)
+                info "Unknown option '$1', ignoring it" $red
+                ;;
+        esac
+        shift
+    done
+}
 
 check_necessary() {
     #test that the path contains android SDK and android NDK as required
@@ -28,12 +64,16 @@ compilation_after() {
     info "Cleaning tremor directory..."
     cd $rootPath/sac/libs/tremor; git checkout *; cd - 1>/dev/null
 
-    #android update project -p . -t "android-8" -n $nameUpper --subprojects
-    #ant debug
+    if [ $UPDATE_JAVA = 1 ]; then
+        info "TODO" $red
+        # info "Updating android project"
+        # android update project -p . -t "android-8" -n $gameName --subprojects
+        # ant debug
+    fi
 }
 
 launch_the_application() {
-    if [ ! -z $(echo $1 | grep i) ]; then
+    if [ $INSTALL_ON_DEVICE = 1 ]; then
         info "Installing on device ..."
         ant installd -e
     fi
@@ -41,12 +81,12 @@ launch_the_application() {
     if [ ! -z $(echo $1 | grep r) ]; then
         info "Running app $gameName..."
 
-        gameNameLower=$(echo $gameName | sed 's/\u&/^./')
+        gameNameLower=$(echo $gameName | sed 's/^./\l&/')
 
-        adb shell am start -n net.damsy.soupeaucaillou.$gameNameLower/net.damsy.soupeaucaillou.$gameNameLower.${gameName}Activity
+        adb shell am start -n net.damsy.soupeaucaillou.$gameNameLower/.${gameName}Activity
     fi
 
-    if [ ! -z $(echo $1 | grep l) ]; then
+    if [ $RUN_LOGCAT = 1 ]; then
         info "Launching adb logcat..."
         adb logcat -C | grep sac
     fi
