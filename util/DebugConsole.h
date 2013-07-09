770c9068c906963d86d364899c69538825442a58
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2013-07-09 19:50:45 +0200

    Remove define macros since there are pretty useless, and add some docs...

--- util/DebugConsole.h
+++ util/DebugConsole.h
@@ -8,33 +8,33 @@
 #include <AntTweakBar.h>
 
 
-//use this method to add a method to the debug console with an enum argument
-#define REGISTER_ONE_ARG(name, args)\
-    static int name##Value = args[0].Value;\
-    DebugConsole::Instance().registerMethodWithOneArg(#name, #args, &callback##name, args, sizeof(args)/sizeof(TwEnumVal), &name##Value);
-
-//use this method to add a method to the debug console without arg
-#define REGISTER_NO_ARG(name)\
-    DebugConsole::Instance().registerMethodWithoutArg(#name, &callback##name);
-
-
-
 class DebugConsole {
     public:
-        //called in LevelEditor
+        //called in LevelEditor, creating "DebugConsole" icon in Ant
         void initTW();
 
         static DebugConsole & Instance();
 
         //there is no argument
-        static void registerMethodWithoutArg(const std::string & name, void (*callback)(void*));
+        static void registerMethodWithoutArg(const std::string & functionName, void (*callback)(void*));
 
         //user can choose the argument in a enum list
-        static void registerMethodWithOneArg(const std::string & name, const std::string & argumentName,
+        //functionName: string to be displayed in Ant TW bar
+        //argumentName: string to be displayed in Ant TW bar (for the argument)
+        //callback: function to be called when clicking the function in Ant
+        //availableArgs: enum list of possible values
+        //availableArgsSize: size of the enum (note: you can use sizeof(availableArgs)/sizeof(TWEnumVal))
+        //storingPlace: where to store the argument value (should be a static or allocated variable)
+        static void registerMethodWithOneArg(const std::string & functionName, const std::string & argumentName,
             void (*callback)(void*), TwEnumVal* availableArgs, unsigned availableArgsSize, void* storingPlace);
 
         //user can choose the argument value of the specified type (see http://anttweakbar.sourceforge.net/doc/tools:anttweakbar:twtype for the whole list)
-        static void registerMethodWithOneArg(const std::string & name, const std::string & argumentName,
+        //functionName: string to be displayed in Ant TW bar
+        //argumentName: string to be displayed in Ant TW bar (for the argument)
+        //callback: function to be called when clicking the function in Ant
+        //type: the argument type (bool, string, int, float, ... see http://anttweakbar.sourceforge.net/doc/tools:anttweakbar:twtype for the whole list)
+        //storingPlace: where to store the argument value (should be a static or allocated variable)
+        static void registerMethodWithOneArg(const std::string & functionName, const std::string & argumentName,
             void (*callback)(void*), void* storingPlace, TwType type);
 
     private:
