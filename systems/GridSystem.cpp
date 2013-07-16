3a81dfcc2508c8924725283373464e34392b67e7
Pierre-Eric Pelloux-Prayer <pierre-eric@damsy.net>
2013-07-16 17:47:12 +0200

    grid: declare properties properly

--- systems/GridSystem.cpp
+++ systems/GridSystem.cpp
@@ -4,7 +4,10 @@ INSTANCE_IMPL(GridSystem);
 
 GridSystem::GridSystem() : ComponentSystemImpl<GridComponent>("Grid") {
     GridComponent tc;
-    componentSerializer.add(new Property<bool>("type", OFFSET(type, tc)));
+    componentSerializer.add(new Property<int>("type", OFFSET(type, tc)));
+    componentSerializer.add(new Property<bool>("blocks_path", OFFSET(blocksPath, tc)));
+    componentSerializer.add(new Property<bool>("blocks_vision", OFFSET(blocksVision, tc)));
+    componentSerializer.add(new Property<bool>("can_be_on_multiple_cells", OFFSET(canBeOnMultipleCells, tc)));
 }
 
 void GridSystem::DoUpdate(float) {
