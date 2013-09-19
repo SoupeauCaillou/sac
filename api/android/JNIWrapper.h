059e9aca5276a828e69ee864d30c930dfbb6e7a3
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2013-09-19 21:29:59 +0200

    Updated headers manually (:D)

--- api/android/JNIWrapper.h
+++ api/android/JNIWrapper.h
@@ -1,3 +1,25 @@
+/*
+    This file is part of Soupe Au Caillou.
+
+    @author Soupe au Caillou - Jordane Pelloux-Prayer
+    @author Soupe au Caillou - Gautier Pelloux-Prayer
+    @author Soupe au Caillou - Pierre-Eric Pelloux-Prayer
+
+    Soupe Au Caillou is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3.
+
+    Soupe Au Caillou is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Soupe Au Caillou.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
 #pragma once
 
 #include <jni.h>
--- base/NamedAssetLibrary.h
+++ base/NamedAssetLibrary.h
@@ -1,21 +1,25 @@
 /*
-    This file is part of sac.
+    This file is part of Soupe Au Caillou.
 
-    @author Soupe au Caillou - Pierre-Eric Pelloux-Prayer
+    @author Soupe au Caillou - Jordane Pelloux-Prayer
     @author Soupe au Caillou - Gautier Pelloux-Prayer
+    @author Soupe au Caillou - Pierre-Eric Pelloux-Prayer
 
-    Heriswap is free software: you can redistribute it and/or modify
+    Soupe Au Caillou is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, version 3.
 
-    Heriswap is distributed in the hope that it will be useful,
+    Soupe Au Caillou is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Heriswap.  If not, see <http://www.gnu.org/licenses/>.
+    along with Soupe Au Caillou.  If not, see <http://www.gnu.org/licenses/>.
 */
+
+
+
 #pragma once
 
 #include <map>
