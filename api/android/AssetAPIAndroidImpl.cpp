0d89ca29e91058217f99f8e9abc40edd625182cf
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2013-09-29 16:20:22 +0200

    Temporary implement AssetAPIAndroidImpl::LoadFile method (copy/paste from Linux's one). Needs to be refactorised...

--- api/android/AssetAPIAndroidImpl.cpp
+++ api/android/AssetAPIAndroidImpl.cpp
@@ -94,9 +94,28 @@ const std::string &  AssetAPIAndroidImpl::getWritableAppDatasPath() {
     return path;
  }
 
-FileBuffer AssetAPIAndroidImpl::loadFile(const std::string&) {
-    LOGT("Extract Linux impl as static method and use it here");
-    return FileBuffer();
+FileBuffer AssetAPIAndroidImpl::loadFile(const std::string& full) {
+    LOGT("Refactor with Linux since they are sharing everything");
+    FileBuffer fb;
+    fb.data = 0;
+    FILE* file = fopen(full.c_str(), "rb");
+
+    if (! file) {
+        return fb;
+    }
+
+    fseek(file, 0, SEEK_END);
+    fb.size = ftell(file);
+    rewind(file);
+    fb.data = new uint8_t[fb.size + 1];
+    int count = 0;
+    do {
+        count += fread(&fb.data[count], 1, fb.size - count, file);
+    } while (count < fb.size);
+
+    fclose(file);
+    fb.data[fb.size] = 0;
+    return fb;
 }
 
 std::list<std::string> AssetAPIAndroidImpl::listContent(const std::string&, const std::string&, const std::string&) {
