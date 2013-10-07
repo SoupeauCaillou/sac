b636fe27e67f6115ca037775d82ca0285a6052b8
Jordane Pelloux-Prayer <jordane@damsy.net>
2013-10-07 21:41:35 +0200

    Rewind to the generic version

--- tools/extract_xcf_layers.py
+++ tools/extract_xcf_layers.py
@@ -5,57 +5,32 @@ import os, glob, sys, time
 
 allowedFormat = [".xcf", ".psd"]
 
-DESC = "[General]\n"\
-	   "page_count = {}\n"
-
-PAGE = "[page_{}]\n"\
-	   "element_count = {}\n"
-
-ELEMENT = "element_{0}_texture = {1}\n"\
-		  "element_{0}_size = {2}, {3}\n"\
-		  "element_{0}_position = {4}, {5}\n"
-
-OTHER = "[others]\n"\
-		"element_count = {}\n"
-
-
 def run(directory, rewrite = False):
 	from gimpfu import pdb
 	start = time.time()
 	#for all xcf files in working directory
-	print("Running on directory '{}'".format(directory))
+	print("Running on directory '{0}'".format(directory))
 	for filename in os.listdir(directory):
-		if os.path.splitext(filename)[-1] in allowedFormat:
-			print("Found a file : '{}'".format(filename))
+		if filename[-4:] in allowedFormat:
+			print("Found a file : '{0}'".format(filename))
 			image = pdb.gimp_file_load(os.path.join(directory, filename), os.path.join(directory, filename))
-			groupLayer = {'others':[]}
-			for layer in image.layers:
-				for c in [layer] + layer.children:
-					if c.name[:-1].isdigit():
-						if c.name[:-1] not in groupLayer:
-							groupLayer[c.name[:-1]] = []
-						groupLayer[c.name[:-1]].append([c.name, c.width, c.height, c.offsets[0], c.offsets[1]])
-					else:
-						groupLayer['others'].append([c.name, c.width, c.height, c.offsets[0], c.offsets[1]])
 
-					print("Write layer '{0}' in '{0}.png'".format(c.name))
-					# write the new png file
-					pdb.file_png_save(image, c, os.path.join(directory, c.name +".png"), os.path.join(directory, c.name +".png"), 0, 9, 0, 0, 0, 0, 0)
-			with open(os.path.join(directory, os.path.splitext(filename)[0] + ".desc"), "wb") as descfile:
-				descfile.write(DESC.format(len(groupLayer)))
+			with open(os.path.join(directory, "infos.desc"), "wb") as f:
+				for layer in image.layers:
+					for c in [layer] + layer.children:
+						f.write("texture = {}\n"\
+								"size = {}, {}\n"\
+								"position = {}, {}\n".format(c.name, c.width, c.height, *c.offsets))
 
-				for key in sorted(groupLayer.keys()):
-					if key == 'others':
-						descfile.write(OTHER.format(key, len(groupLayer[key])))
-					else : 
-						descfile.write(PAGE.format(key, len(groupLayer[key])))
-					element_count = 1
-					for element in groupLayer[key]:
-						descfile.write(ELEMENT.format(element_count, *element))
-						element_count += 1
+						print("Write layer '{0}' in '{0}.png'".format(c.name))
+						# write the new png file
+						pdb.file_png_save(image, c, os.path.join(directory, c.name +".png"), 
+													os.path.join(directory, c.name +".png"), 
+													0, 9, 0, 0, 0, 0, 0)
 
 	end = time.time()
 	print("Finished, total processing time : {0:.{1}f}".format(end-start, 2))
+
 
 if __name__ == '__main__':
 	directory = os.getcwd() + '/'
