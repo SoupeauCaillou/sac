036f40e599af1e4bd901c715e6fd51118637b285
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2013-12-15 12:39:53 +0100

    Added option -G to generate ninja/makefile and sublime files

--- tools/build/build-all.sh
+++ tools/build/build-all.sh
@@ -29,7 +29,7 @@ source ../coolStuff.sh
 
 #get the list of available targets (need to be displayed in OPTIONS help
 TARGETS_LIST=$(grep SUPPORTED_TARGETS ../../build/cmake/CMakeLists.txt | head -1 | cut -d " " -f 2- | tr -d ')')
-
+BUILD_SYSTEM_LIST="ninja makefile"
 #how to use the script
 export SAC_USAGE="$0 [options]"
 export SAC_OPTIONS="\
@@ -41,6 +41,7 @@ You can also specify arguments:
 \t-h|-help: show this help
 \t-release|-debug: build type specifying
 \t--t|--target target_name: specify the target name (in the following list: '$TARGETS_LIST')
+\t--g|--generate ${BUILD_SYSTEM_LIST// /|}: generate build system files and sublime project
 \t--c|--cmakeconfig \"arguments for cmake\": see cmake for options."
 
 
@@ -52,44 +53,52 @@ export SAC_EXAMPLE="${green}TODO${default_color}"
 	fi
 
 ######### 1 : Read arguments. #########
-    CMAKE_CONFIG=""
+    CMAKE_CONFIG=()
     TARGETS=""
     RUN_ARGS=""
     COLOREDLOGS_ARGS=""
     CMAKE_BUILD_TARGET=""
     SHOULD_QUIT_AFTER_READ=0
-    ARGS=$@
+    BUILD_SYSTEM=""
+    ARGS="" #DONT DID ARGS=$@ nor ARGS="$@" because it'll mess up variables containing spaces
     while [ "$1" != "" ]; do
         case $1 in
             "-h" | "-help")
                 SHOULD_QUIT_AFTER_READ=1
                 ;;
-            "--c" | "--cmakeconfig")
-                shift
-                TARGETS=$TARGETS"n"
-                CMAKE_CONFIG=$CMAKE_CONFIG" $1"
-                ;;
             "-release")
-                CMAKE_CONFIG=$CMAKE_CONFIG" -DCMAKE_BUILD_TYPE=release"
+                CMAKE_CONFIG+=(-DCMAKE_BUILD_TYPE=release)
                 ;;
             "-debug")
-                CMAKE_CONFIG=$CMAKE_CONFIG" -DCMAKE_BUILD_TYPE=debug"
+                CMAKE_CONFIG+=(-DCMAKE_BUILD_TYPE=debug)
                 ;;
              "--t" | "--target")
                 shift
                 CMAKE_BUILD_TARGET=$1
                 ;;
-            --*)
+            "--c" | "--cmakeconfig")
                 shift
+                TARGETS=$TARGETS"n"
+                CMAKE_CONFIG+=($1)
+                ;;
+            "--g")
+                shift
+                TARGETS=$TARGETS"n"
+                BUILD_SYSTEM=$1
+                ;;
+            --*)
+                ARGS+=" "$1
+                shift
+                ARGS+=" "$1
                 ;;
             -*)
+                ARGS+=" "$1
                 ;;
             *)
                 TARGETS=$TARGETS$1
         esac
         shift
     done
-
     #if we didn't ask for help, get the target config
     if [ $SHOULD_QUIT_AFTER_READ = 0 ]; then
         # if build target is empty, ask user for it
@@ -104,11 +113,11 @@ export SAC_EXAMPLE="${green}TODO${default_color}"
         #convert to lowercase
         CMAKE_BUILD_TARGET=$(echo $CMAKE_BUILD_TARGET | tr '[:upper:]' '[:lower:]')
         #save for cmake
-        CMAKE_CONFIG=$CMAKE_CONFIG" -DTARGET=$CMAKE_BUILD_TARGET"
+        CMAKE_CONFIG+=(-DTARGET=$CMAKE_BUILD_TARGET)
         #import the script
         source build-$CMAKE_BUILD_TARGET.sh
 
-        #let him parse the args if necessary
+        #let it parse the args if necessary
         parse_arguments $ARGS
     fi
 
@@ -124,8 +133,8 @@ export SAC_EXAMPLE="${green}TODO${default_color}"
 
     # retrieve build type
     CMAKE_BUILD_TYPE="debug"
-    if [ ! -z "$(grep -i -- '-DCMAKE_BUILD_TYPE=' <<< "$CMAKE_CONFIG")" ]; then
-        CMAKE_BUILD_TYPE=$(echo $CMAKE_CONFIG | sed 's/-DCMAKE_BUILD_TYPE=/~/' | cut -d '~' -f2 | cut -d ' ' -f1)
+    if [ ! -z "$(grep -i -- '-DCMAKE_BUILD_TYPE=' <<< "${CMAKE_CONFIG[@]}")" ]; then
+        CMAKE_BUILD_TYPE=$(echo ${CMAKE_CONFIG[@]} | sed 's/-DCMAKE_BUILD_TYPE=/~/' | cut -d '~' -f2 | cut -d ' ' -f1)
     fi
 
     builddir=$rootPath/build/$CMAKE_BUILD_TARGET-$CMAKE_BUILD_TYPE
@@ -157,17 +166,38 @@ export SAC_EXAMPLE="${green}TODO${default_color}"
 	if [ ! -z "$(echo $TARGETS | grep -e n)" ]; then
         info "Compiling.."
 
+        if [ ! -z "$BUILD_SYSTEM" ]; then
+            REAL_NAME=""
+            if ! [[ " $BUILD_SYSTEM_LIST " =~ " $BUILD_SYSTEM " ]]; then
+                error_and_quit "Unknown build system '$BUILD_SYSTEM'."
+            elif [ "$BUILD_SYSTEM" = "ninja" ]; then
+                check_package ninja ninja-build
+                REAL_NAME="Ninja"
+            elif [ "$BUILD_SYSTEM" = "makefile" ]; then
+                REAL_NAME="Unix Makefiles"
+            fi
+            CMAKE_CONFIG+=(-G "Sublime Text 2 - $REAL_NAME")
+        fi
+
         compilation_before
 
-        if (!(cmake $CMAKE_CONFIG $rootPath)); then
+        if ! (cmake "${CMAKE_CONFIG[@]}" $rootPath); then
             compilation_after
             error_and_quit "Error in cmake. Maybe should run with C option?"
         fi
 
-        check_package "make"
-        if (!(make -j4)); then
+        if [ -f Makefile ]; then
+            BUILD_COMMAND='make'
+        elif [ -f build.ninja ]; then
+            BUILD_COMMAND='ninja'
+        else
+            error_and_quit 'No makefile/build.ninja found in $PWD'
+        fi
+        
+        check_package $BUILD_COMMAND
+        if ! $BUILD_COMMAND; then
             compilation_after
-            error_and_quit "Error in make"
+            error_and_quit "Error when building with '$BUILD_COMMAND'"
         fi
 
         compilation_after
