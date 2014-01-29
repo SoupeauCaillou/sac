f08922aebbd17da2d94c09a8c2cf987826fd69ba
Pierre-Eric Pelloux-Prayer <pierre-eric@damsy.net>
2014-01-29 20:34:24 +0100

    render: remove leading sign bit of z for key creation

--- systems/RenderingSystem.cpp
+++ systems/RenderingSystem.cpp
@@ -215,7 +215,7 @@ static uint64_t makeKeyBlended(const RenderingSystem::RenderCommand& rc) {
     uint64_t key = 0;
 
     // z:       63...51
-    uint64_t s = (((uint64_t)(rc.zi)) >> 19); // 13 bits
+    uint64_t s = (((uint64_t)(rc.zi)) >> 18); // 14 bits, but we remove the leading sign bit
     key |= s << 51;
     // flags:   50...48
     key |= ((uint64_t)(rc.flags & 0x7)) << 48;
@@ -228,51 +228,16 @@ static uint64_t makeKeyBlended(const RenderingSystem::RenderCommand& rc) {
     return key;
 }
 
-
-// The goal of this sort function is to group sprites to reduce OpenGL state changes.
-static bool sortToMinizeStateChanges(const RenderingSystem::RenderCommand& r1, const RenderingSystem::RenderCommand& r2) {
-    // 1st compare state (flags)
-    if (r1.flags == r2.flags) {
-        // 2nd: compare shader
-        if (r1.effectRef == r2.effectRef) {
-            // 3rd : compare texture
-            if (r1.texture == r2.texture) {
-                // 4th : compare color
-                return r1.color < r2.color;
-            } else {
-                return r1.texture < r2.texture;
-            }
-        } else {
-            return r1.effectRef < r2.effectRef;
-        }
-    } else {
-        return r1.flags < r2.flags;
-    }
-}
-
 // This function is used to sort opaque sprites from front to back
 // Note: the sort algorithm sort from min to max, so in this case, r1 < r2
 // means r1.z > r2.z
 static bool sortFrontToBack(const RenderingSystem::RenderCommand& r1, const RenderingSystem::RenderCommand& r2) {
     return r1.key < r2.key;
-    static const double EPSILON = 0.0001;
-    if (glm::abs(r1.z - r2.z) <= EPSILON) {
-        return sortToMinizeStateChanges(r1, r2);
-    } else {
-        return r1.z > r2.z;
-    }
 }
 
 // This function is used to sort alpha-blended sprites from back to front.
 static bool sortBackToFront(const RenderingSystem::RenderCommand& r1, const RenderingSystem::RenderCommand& r2) {
     return r1.key < r2.key;
-    static const double EPSILON = 0.0001;
-    if (glm::abs(r1.z - r2.z) <= EPSILON) {
-        return sortToMinizeStateChanges(r1, r2);
-    } else {
-        return r1.z < r2.z;
-    }
-
 }
 
 static inline void modifyQ(RenderingSystem::RenderCommand& r, const glm::vec2& offsetPos, const glm::vec2& size) {
