cd697e0a9102f2e865ae8c806cd1f6ac5e07f290
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2014-03-06 11:06:54 +0100

    [Android] Automatically generate plugins.xml file from project.properties

--- build/cmake/toolchains/android.cmake
+++ build/cmake/toolchains/android.cmake
@@ -14,26 +14,67 @@ SET (SAC_LIB_TYPE SHARED)
 
 include_directories(${PROJECT_SOURCE_DIR}/sac/libs/)
 
-# #enable GDB debug in debug mode
-# if (BUILD_TARGET STREQUAL "DEBUG")
-#     # thanks to http://www.rojtberg.net/465/debugging-native-code-with-ndk-gdb-using-standalone-cmake-toolchain/
-#     if (NOT ANDROID_NDK)
-#         set (ANDROID_NDK $ENV{ANDROID_NDK})
-#     endif()
+#######################################################################
+###########################Enable GDB debug###########################
+#######################################################################
+if (0 AND BUILD_TARGET STREQUAL "DEBUG")
+    # thanks to http://www.rojtberg.net/465/debugging-native-code-with-ndk-gdb-using-standalone-cmake-toolchain/
+    if (NOT ANDROID_NDK)
+        set (ANDROID_NDK $ENV{ANDROID_NDK})
+    endif()
 
-#     # 1. generate Android.mk
-#     file(WRITE ${PROJECT_SOURCE_DIR}/jni/Android.mk "APP_ABI := ${ANDROID_NDK_ABI_NAME}\n")
+    # 1. generate Android.mk
+    file(WRITE ${PROJECT_SOURCE_DIR}/jni/Android.mk "APP_ABI := ${ANDROID_NDK_ABI_NAME}\n")
 
-#     # 2. generate gdb.setup
-#     get_directory_property(INCLUDE_DIRECTORIES DIRECTORY ${PROJECT_SOURCE_DIR} INCLUDE_DIRECTORIES)
-#     string(REGEX REPLACE ";" " " INCLUDE_DIRECTORIES "${INCLUDE_DIRECTORIES}")
-#     file(WRITE ${PROJECT_SOURCE_DIR}/libs/${ANDROID_NDK_ABI_NAME}/gdb.setup "set solib-search-path ${PROJECT_SOURCE_DIR}/obj/local/${ANDROID_NDK_ABI_NAME}\n")
-#     file(APPEND ${PROJECT_SOURCE_DIR}/libs/${ANDROID_NDK_ABI_NAME}/gdb.setup "directory ${INCLUDE_DIRECTORIES}\n")
+    # 2. generate gdb.setup
+    get_directory_property(INCLUDE_DIRECTORIES DIRECTORY ${PROJECT_SOURCE_DIR} INCLUDE_DIRECTORIES)
+    string(REGEX REPLACE ";" " " INCLUDE_DIRECTORIES "${INCLUDE_DIRECTORIES}")
+    file(WRITE ${PROJECT_SOURCE_DIR}/libs/${ANDROID_NDK_ABI_NAME}/gdb.setup "set solib-search-path ${PROJECT_SOURCE_DIR}/obj/local/${ANDROID_NDK_ABI_NAME}\n")
+    file(APPEND ${PROJECT_SOURCE_DIR}/libs/${ANDROID_NDK_ABI_NAME}/gdb.setup "directory ${INCLUDE_DIRECTORIES}\n")
 
-#     # 3. copy gdbserver executable
-#     file(COPY ${ANDROID_NDK}/prebuilt/android-arm/gdbserver/gdbserver DESTINATION ${PROJECT_SOURCE_DIR}/libs/${ANDROID_NDK_ABI_NAME}/)
-# endif()
+    # 3. copy gdbserver executable
+    file(COPY ${ANDROID_NDK}/prebuilt/android-arm/gdbserver/gdbserver DESTINATION ${PROJECT_SOURCE_DIR}/libs/${ANDROID_NDK_ABI_NAME}/)
+endif()
 
+
+#######################################################################
+#########Generate plugins list needed for the application##############
+#######################################################################
+    #clean file
+    file (WRITE "${PROJECT_SOURCE_DIR}/res/values/plugins.xml" 
+        "<?xml version='1.0' encoding='UTF-8'?>\n<resources>\n\t<string-array name='plugins_list'>\n")
+
+    # get the list of plugins bundled with the APK, in project.properties file
+    execute_process(
+        COMMAND grep android.library.reference project.properties
+        COMMAND grep -v SacFramework
+        COMMAND cut -d= -f 2
+        OUTPUT_VARIABLE PLUGINS_LIST
+        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+    )
+
+    # and find package/class names for each of these plugins
+    foreach (PLUGIN_PATH ${PLUGINS_LIST})
+        # Assume that package name convention is 
+        # net.damsy.soupeaucaillou.[plugin folder in lowercase and without 'sac' prefix ]
+        string (TOLOWER "${PLUGIN_PATH}" plugin_path)
+        string(REGEX REPLACE ".*/sac(.*)\n$" "net.damsy.soupeaucaillou.\\1" PACKAGE_NAME ${plugin_path})
+        
+        # Assume that plugin name convention is
+        # [plugin folder with 'Plugin' suffix]
+        string(REGEX REPLACE ".*/(.*)\n$" "\\1Plugin" CLASS_NAME ${PLUGIN_PATH})
+
+        file (APPEND "${PROJECT_SOURCE_DIR}/res/values/plugins.xml"
+            "\t\t<item>${PACKAGE_NAME}.${CLASS_NAME}</item>\n")        
+    endforeach()
+
+    file (APPEND "${PROJECT_SOURCE_DIR}/res/values/plugins.xml"
+        "\t</string-array>\n</resources>\n")
+
+
+#######################################################################
+#######Declaration of functions needed by the main CMakeLists.txt######
+#######################################################################
 function (get_platform_dependent_sources)
     file(
         GLOB_RECURSE platform_source_files
