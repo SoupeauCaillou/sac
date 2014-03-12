2093f1e6b2af1717fedff52a9d2e516245cf8c8e
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2014-03-12 16:48:50 +0100

    [Script] Fix output path for generate atlas script

--- tools/generate_atlas.sh
+++ tools/generate_atlas.sh
@@ -13,7 +13,7 @@ whereAmI=$(cd "$(dirname "$0")" && pwd)
 if [ -h $0 ]; then
     whereAmI+=/$(dirname $(readlink $0))
 fi
-outPath="$current/.."
+outPath="$whereAmI/../.."
 
 #import cool stuff
 source $whereAmI/cool_stuff.sh
