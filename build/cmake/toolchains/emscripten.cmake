618581cbcb7522f1a2e281d2f59667feeea4143e
Pierre-Eric Pelloux-Prayer <pierre-eric@damsy.net>
2014-06-11 15:06:29 +0200

    emscripten: don't bundle png

--- build/cmake/toolchains/emscripten.cmake
+++ build/cmake/toolchains/emscripten.cmake
@@ -47,7 +47,7 @@ function (postbuild_specific_actions)
         TARGET ${EXECUTABLE_NAME} PRE_LINK
         COMMAND rm -rf assets
         COMMAND mkdir assets
-        COMMAND cp -r ${PROJECT_SOURCE_DIR}/assets/* ${PROJECT_SOURCE_DIR}/assetspc/* ${PROJECT_SOURCE_DIR}/res/* assets
+        COMMAND cp -r ${PROJECT_SOURCE_DIR}/assets/* ${PROJECT_SOURCE_DIR}/res/* assets
         COMMAND find assets/ -name '*pvr*' -or -name '*pkm*' -exec rm -r {} '\;'
         COMMAND rm -r assets/ldpi assets/mdpi
         WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
