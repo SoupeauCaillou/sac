e533161f9bed66c5487eb683f10241a0f6db147c
Pierre-Eric Pelloux-Prayer <pierre-eric@damsy.net>
2014-08-26 14:38:17 +0200

    EntityBatch class allows batch processing of entities

---
+++ util/EntityBatch.cpp
@@ -0,0 +1,131 @@
+#include "EntityBatch.h"
+
+#include "systems/ButtonSystem.h"
+#include "systems/RenderingSystem.h"
+#include "systems/TextSystem.h"
+
+EntityBatch::EntityBatch(float in, float out) :
+    action(Action::Idle), mode(ActivationMode::Instantaneous), fadeinDuration(in), fadeoutDuration(out) {
+
+}
+
+void EntityBatch::setDurations(float fadein, float fadeout) {
+    fadeinDuration = fadein;
+    fadeoutDuration = fadeout;
+}
+
+void EntityBatch::addEntity(Entity e) {
+    entities.push_back(e);
+}
+
+void EntityBatch::enable(ActivationMode::Enum m) {
+    action = Action::Enabling;
+    mode = m;
+    accum = 0;
+
+    if (m == ActivationMode::Instantaneous) {
+        EntityBatch::enableEntities(entities);
+    }
+}
+
+void EntityBatch::disable(ActivationMode::Enum m) {
+    action = Action::Disabling;
+    mode = m;
+    accum = 0;
+
+    if (m == ActivationMode::Instantaneous) {
+        EntityBatch::disableEntities(entities);
+    }
+}
+
+bool EntityBatch::update(float dt) {
+    switch (action) {
+        case Action::Idle: return true;
+        case Action::Enabling:
+        case Action::Disabling:
+            if (mode == ActivationMode::Fading) {
+                // progressively show/hide entities
+                float t = 0;
+                if (action == Action::Enabling) {
+                    t = accum / fadeinDuration;
+                } else {
+                    t = accum / fadeoutDuration;
+                }
+
+                if (t >= 1) {
+                    if (action == Action::Enabling) {
+                        EntityBatch::enableEntities(entities);
+                    } else {
+                        EntityBatch::disableEntities(entities);
+                    }
+                    action = Action::Idle;
+                    return true;
+                } else {
+                    if (action == Action::Disabling) t = 1 - t;
+
+                    EntityBatch::updateEntities(entities, t);
+                    return false;
+                }
+                accum += dt;
+            } else {
+                action = Action::Idle;
+                return true;
+            }
+        default:
+            action = Action::Idle;
+            return true;
+    }
+}
+
+void EntityBatch::enableEntities(std::vector<Entity>& entities) {
+    for (auto e: entities) {
+        auto* rc = theRenderingSystem.Get(e, false);
+        if (rc)
+            rc->show = true;
+
+        auto* tc = theTextSystem.Get(e, false);
+        if (tc)
+            tc->show = true;
+
+        auto* bc = theButtonSystem.Get(e, false);
+        if (bc)
+            bc->enabled = true;
+    }
+}
+
+void EntityBatch::disableEntities(std::vector<Entity>& entities) {
+    // hide every fading-out entities
+    for (auto e: entities) {
+        auto* rc = theRenderingSystem.Get(e, false);
+        if (rc)
+            rc->show = false;
+
+        auto* tc = theTextSystem.Get(e, false);
+        if (tc)
+            tc->show = false;
+
+        auto* bc = theButtonSystem.Get(e, false);
+        if (bc)
+            bc->enabled = false;
+    }
+}
+
+void EntityBatch::updateEntities(std::vector<Entity>& entities, float alpha) {
+    for (auto e: entities) {
+        auto* rc = theRenderingSystem.Get(e, false);
+        if (rc) {
+            rc->show = true;
+            rc->color.a = alpha;
+        }
+
+        auto* tc = theTextSystem.Get(e, false);
+        if (tc) {
+            tc->show = true;
+            tc->color.a = alpha;
+        }
+
+        auto* bc = theButtonSystem.Get(e, false);
+        if (bc)
+            bc->enabled = false;
+    }
+}
---
+++ util/EntityBatch.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "base/Entity.h"
+#include <vector>
+
+namespace ActivationMode {
+    enum Enum {
+        Instantaneous = 0,
+        Fading
+    };
+}
+
+namespace Action {
+    enum Enum {
+        Idle,
+        Enabling,
+        Disabling
+    };
+}
+
+class EntityBatch {
+    public:
+        EntityBatch(float fadeinDuration = 0.5f, float fadeoutDuration = 0.5f);
+
+        void setDurations(float fadein, float fadeout);
+        void addEntity(Entity e);
+
+        void enable(ActivationMode::Enum mode = ActivationMode::Instantaneous);
+        void disable(ActivationMode::Enum mode = ActivationMode::Instantaneous);
+        bool update(float dt);
+
+        static void enableEntities(std::vector<Entity>& entities);
+        static void disableEntities(std::vector<Entity>& entities);
+        static void updateEntities(std::vector<Entity>& entities, float alpha);
+
+    private:
+        Action::Enum action;
+        ActivationMode::Enum mode;
+        std::vector<Entity> entities;
+        float fadeinDuration, fadeoutDuration;
+        float accum;
+};
