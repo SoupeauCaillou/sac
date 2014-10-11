d2d2670fd2ab93e947420c3d8491cccd3b18ca04
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2014-10-11 12:27:09 -0400

    Remove coloredlog script since there are already colored

--- tools/build/build-linux.sh
+++ tools/build/build-linux.sh
@@ -3,15 +3,12 @@ export PLATFORM_OPTIONS="\
 \tn: simply compile
 \tr: run the app\t\t\t\t\t(options available, see below)
 \td: run&debug the app with cgdb
-\tl: use colorlog.sh script for colored logs\t(options available, see below)
 You can also specify arguments:
-\t--l|--log \"arguments for coloredlog script\": options for this script. See it for arguments availables
 \t--r|--run \"arguments for game\": arguments handled by the game (--restore, --verbose, ..., whatever you did!)"
 
 parse_arguments() {
     ret=0
     run_args=""
-    coloredlogs_args=""
     while [ "$1" != "" ]; do
         case $1 in
             #ignore higher level commands
@@ -24,11 +21,6 @@ parse_arguments() {
                 shift
                 ;;
 
-            "--l" | "--log")
-                shift
-                targets=$targets"l"
-                coloredlogs_args=$1
-                ;;
             "--r" | "--run")
                 shift
                 targets=$targets"r"
@@ -75,22 +67,8 @@ launch_the_application() {
     #launch required
     elif [ ! -z "$(echo $targets | grep r)" ]; then
         #verbose required
-        if [ ! -z "$(echo $targets | grep l)" ]; then
-            info "Launch with colored log."
-
-            #coloredlogs shouldn't be empty
-            if [ -z "$coloredlogs_args" ]; then
-                info "No arg for color script ?\nChoose tag with option -l" $red
-                info "I will use 'all' tag here"
-                coloredlogs_args='all'
-                sleep 3
-            fi
-            $executable | $rootPath/sac/tools/build/linux-coloredLogs.sh $coloredlogs_args
-
-        else
-            info "Launch game."
-            $executable
-        fi
+        info "Launch game."
+        $executable
     fi
 }
 
