cccf1d9a1a75684ad8d95943af643f007aa66be2
Pierre-Eric Pelloux-Prayer <pierre-eric@damsy.net>
2014-11-18 21:03:42 +0100

    random: can now use external generator

--- util/Random.cpp
+++ util/Random.cpp
@@ -30,25 +30,42 @@ void Random::Init (unsigned int seed) {
 }
 
 float Random::Float (float min, float max) {
+    return Float(mt, min, max);
+}
+
+float Random::Float (std::mt19937& gen, float min, float max) {
     std::uniform_real_distribution<float> dist(min, max);
-    return dist(mt);
+    return dist(gen);
 }
 
 void Random::N_Floats (int n, float* out, float min, float max) {
+    N_Floats(mt, n, out, min, max);
+}
+
+void Random::N_Floats (std::mt19937& gen, int n, float* out, float min, float max) {
     std::uniform_real_distribution<float> dist(min, max);
     for (int i=0; i<n; i++) {
-        out[i] = dist(mt);
+        out[i] = dist(gen);
     }
 }
 
 int Random::Int (int min, int max) {
-    std::uniform_int_distribution<int> dist(min, max);
-    return dist(mt);
+    return Int(mt, min, max);
 }
 
+int Random::Int (std::mt19937& gen, int min, int max) {
+    std::uniform_int_distribution<int> dist(min, max);
+    return dist(gen);
+}
+
+
 void Random::N_Ints (int n, int* out, int min, int max) {
+    N_Ints(mt, n, out, min, max);
+}
+
+void Random::N_Ints (std::mt19937& gen, int n, int* out, int min, int max) {
     std::uniform_int_distribution<int> dist(min, max);
     for (int i=0; i<n; i++) {
-        out[i] = dist(mt);
+        out[i] = dist(gen);
     }
 }
