510a33e0582e20670c95f8fd35f07db6959040a9
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2015-01-17 22:30:06 +0100

    Use -g3 in DEBUG to get preprocessor information and remove -g from RELEASE mode

--- cmake/toolchains/linux.cmake
+++ cmake/toolchains/linux.cmake
@@ -3,8 +3,8 @@ add_definitions(-DSAC_ASSETS_DIR="${CMAKE_SOURCE_DIR}/assets/")
 
 # Enable '-D_GLIBCXX_DEBUG' to debug stl containers related issues
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x -pthread -g -Wall -W -Werror")
-set(CXX_FLAGS_DEBUG "-O0 -Wno-unused-parameter")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x -pthread -Wall -W -Werror")
+set(CXX_FLAGS_DEBUG "-O0 -Wno-unused-parameter -g3")
 set(CXX_FLAGS_RELEASE "-O3 -fconstexpr-depth=10000")
 
 if (${BUILD_TARGET} STREQUAL "DEBUG")
